692fdc7703764a02bffdd67aeea6a4b9047b30ec
Nicholas Reinicke <Nicholas.Reinicke@nrel.gov>
2023-04-17 16:53:29 -0600

    add tracking print statements

--- scripts/build_rust_map.py
+++ scripts/build_rust_map.py
@@ -4,20 +4,26 @@ import pickle
 from nrel.routee.compass.rust.rust_map import build_rust_map_from_gdf
 
 if __name__ == "__main__":
+    print("loading road network links..")
     gdf = pd.read_pickle("/projects/mbap/amazon_eco/us_network.pickle")
     weight_restrictions_file = "/projects/mbap/amazon_eco/weight_restrictions.pickle"
+    print("loading weight restrictions..")
     with open(weight_restrictions_file, "rb") as f:
         weight_restrictions = pickle.load(f)
     height_restrictions_file = "/projects/mbap/amazon_eco/height_restrictions.pickle"
+    print("loading height restrictions..")
     with open(height_restrictions_file, "rb") as f:
         height_restrictions = pickle.load(f)
     width_restrictions_file = "/projects/mbap/amazon_eco/width_restrictions.pickle"
+    print("loading width restrictions..")
     with open(width_restrictions_file, "rb") as f:
         width_restrictions = pickle.load(f)
     length_restrictions_file = "/projects/mbap/amazon_eco/length_restrictions.pickle"
+    print("loading length restrictions..")
     with open(length_restrictions_file, "rb") as f:
         length_restrictions = pickle.load(f)
 
+    print("building rust map..")
     rust_map = build_rust_map_from_gdf(
         gdf,
         weight_restrictions=weight_restrictions,
