1c072d8abb1f7a690e16a2f61c3c5729cfc9680a
rfitzger <robert.fitzgerald@nrel.gov>
2023-05-03 11:31:56 -0600

    revision on sketch of traversal model

--- rust/src/model/traversal/traversal_model.rs
+++ rust/src/model/traversal/traversal_model.rs
@@ -1,6 +1,6 @@
 use crate::{
     algorithm::search::min_search_tree::frontier::Frontier,
-    model::{cost::cost::Cost, graph::edge_id::EdgeId, property::edge::Edge},
+    model::{cost::cost::Cost, property::edge::Edge},
 };
 
 use super::traversal_error::TraversalError;
@@ -8,18 +8,22 @@ use super::traversal_error::TraversalError;
 pub trait TraversalModel {
     type State;
 
-    fn initial_state(&self, e: Edge) -> Result<Self::State, TraversalError>;
+    fn initial_state(&self) -> Result<Self::State, TraversalError>;
 
-    fn traversal_cost(&self, e: Edge) -> Result<Cost, TraversalError>;
+    fn traversal_cost(
+        &self,
+        e: &Edge,
+        state: &Self::State,
+    ) -> Result<(Cost, Self::State), TraversalError>;
 
-    fn access_cost(&self, src: Edge, dst: Edge) -> Result<Cost, TraversalError>;
+    fn access_cost(
+        &self,
+        src: &Edge,
+        dst: &Edge,
+        state: &Self::State,
+    ) -> Result<(Cost, Self::State), TraversalError>;
 
-    fn update(&self, s: Self::State, c: Cost) -> Result<Self::State, TraversalError>;
+    // fn update(&self, s: Self::State, c: Cost) -> Result<Self::State, TraversalError>;
 
-    // todo: provide some acceptably-scoped termination function
-    // fn terminate(
-    //     &self,
-    //     edge_id: EdgeId,
-    //     frontier: Frontier<Self::State>,
-    // ) -> Result<bool, TraversalError>;
+    fn terminate_search(&self, frontier: &Frontier<Self::State>) -> Result<bool, TraversalError>;
 }
