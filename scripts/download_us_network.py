de3749cca1e0f027355aef68490560379240ee9a
Nicholas Reinicke <Nicholas.Reinicke@nrel.gov>
2023-05-10 11:48:52 -0600

    add timers

--- scripts/download_us_network.py
+++ scripts/download_us_network.py
@@ -4,11 +4,12 @@ import geopandas as gpd
 from pathlib import Path
 import sqlalchemy as sql
 import math
+import time
 from datetime import datetime
 from multiprocessing import Pool
 
 CHUNK_SIZE = 5_000_000
-NUM_PROCS = 4 
+NUM_PROCS = 4
 TABLE_NAME = "network_w_speed_profiles"
 WEB_MERCATOR = "epsg:3857"
 
@@ -37,18 +38,25 @@ def download_and_save_chunk(chunk_id):
         log.info(f"Chunk {chunk_id} already exists, skipping")
         return
 
+    log.info(f"Downloading chunk {chunk_id}")
+    start_time = time.time()
     df = gpd.read_postgis(query, con)
+    elapsed_time = time.time() - start_time
+    log.info(f"Chunk {chunk_id} downloaded in {elapsed_time} seconds")
+    log.info(f"Converting {chunk_id} to web mercator..")
+    start_time = time.time()
     df = df.to_crs(WEB_MERCATOR)
+    elapsed_time = time.time() - start_time
+    log.info(f"Chunk {chunk_id} converted in {elapsed_time} seconds")
+    log.info(f"Casting {chunk_id} to correct types..")
+    start_time = time.time()
     df = df.astype(
         {
             "netw_id": str,
             "junction_id_to": str,
             "junction_id_from": str,
             "centimeters": int,
-            "link_direction": int,
             "routing_class": int,
-            "speed_average_pos": int,
-            "speed_average_neg": int,
             "monday_profile_id": str,
             "tuesday_profile_id": str,
             "wednesday_profile_id": str,
@@ -58,8 +66,13 @@ def download_and_save_chunk(chunk_id):
             "sunday_profile_id": str,
         }
     )
+    elapsed_time = time.time() - start_time
+    log.info(f"Chunk {chunk_id} casted in {elapsed_time} seconds")
+    log.info(f"Saving chunk {chunk_id} to {file_path}")
+    start_time = time.time()
     df.to_parquet(file_path, index=False)
-    log.info(f"Chunk {chunk_id} saved to {file_path}")
+    elapsed_time = time.time() - start_time
+    log.info(f"Chunk {chunk_id} saved in {elapsed_time} seconds")
 
 
 engine = sql.create_engine(f"postgresql://{USER}:{PASSWORD}@trolley.nrel.gov:5432/master")
