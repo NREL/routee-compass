6ce7b3410d1c581f2768355368bd1a56d906adab
Nicholas Reinicke <Nicholas.Reinicke@nrel.gov>
2023-05-17 09:53:12 -0600

    expose search input and result to python

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,10 +6,10 @@ pub mod time_of_day_speed;
 
 use algorithm::extract_largest_scc;
 use graph::{Graph, Link, Node};
-use map::RustMap;
+use map::{RustMap, SearchInput, SearchResult};
+use powertrain::VehicleParameters;
 use pyo3::prelude::*;
 use time_of_day_speed::TimeOfDaySpeeds;
-use powertrain::VehicleParameters;
 
 #[pymodule]
 fn compass_rust(_py: Python, m: &PyModule) -> PyResult<()> {
@@ -17,6 +17,8 @@ fn compass_rust(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_class::<Node>()?;
     m.add_class::<Graph>()?;
     m.add_class::<RustMap>()?;
+    m.add_class::<SearchInput>()?;
+    m.add_class::<SearchResult>()?;
     m.add_class::<TimeOfDaySpeeds>()?;
     m.add_class::<VehicleParameters>()?;
     m.add_function(wrap_pyfunction!(extract_largest_scc, m)?)?;
