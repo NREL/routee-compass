33c5bcbd17d3a0ab4e4fe1bd63bf90ea28d6f931
rfitzger <robert.fitzgerald@nrel.gov>
2023-05-24 17:02:12 -0600

    cleanup

--- rust/src/sketch.rs
+++ rust/src/sketch.rs
@@ -1,16 +1,28 @@
-pub fn network_from_edgelist_csv<S>(path: Path, edge_id_col: &str, ...) -> Result<DirectedGraph, GraphError> {
+pub fn network_from_edgelist_csv<S>(
+    path: Path,
+    edge_id_col: &str,
+    ...
+) -> Result<DirectedGraph, GraphError> {
     todo!()
 }
 
-pub fn from_edgelist_csv<S>(path: Path) -> Fn((Vertex, Edge, Vertex, S)) -> Result<Cost, TraversalError> {
+pub fn from_edgelist_csv<S>(
+    path: Path,
+) -> Fn((Vertex, Edge, Vertex, S)) -> Result<Cost, TraversalError> {
     todo!()
 }
 
-pub fn from_edgeedgelist_csv<S>(path: Path) -> Fn((Vertex, Edge, Vertex, Edge, Vertex, S)) -> Result<Cost, TraversalError> {
+pub fn from_edgeedgelist_csv<S>(
+    path: Path,
+) -> Fn((Vertex, Edge, Vertex, Edge, Vertex, S)) -> Result<Cost, TraversalError> {
     todo!()
 }
 
-pub fn from_zonelist_csv<S>(path: Path, v1_col_name: &str, ...) -> Fn((Vertex, Edge, Vertex, Edge, Vertex, S)) -> Result<Cost, TraversalError> {
+pub fn from_zonelist_csv<S>(
+    path: Path,
+    v1_col_name: &str,
+    ...
+) -> Fn((Vertex, Edge, Vertex, Edge, Vertex, S)) -> Result<Cost, TraversalError> {
     todo!()
 }
 
@@ -22,7 +34,7 @@ pub fn init() -> Result<App, AppError> {
     // 5. ok!
 }
 
-pub fn run(app: App, queries: Vec[Queries]) -> Result<Vec[Stuff], AppError> {
+pub fn run(app: App, queries: Vec<Queries>) -> Result<Vec<Stuff>, AppError> {
     // 1. find start vertices from rtree
     // 2. parallelize queries
     // 3. run searches
