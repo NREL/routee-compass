9f3e0f1c5bafc84662dd850b276bbc5c8a664a27
rfitzger <robert.fitzgerald@nrel.gov>
2023-06-06 16:43:14 -0600

    comment hints

--- rust/compass-tomtom/src/io/speed/tomtom_speeds.rs
+++ rust/compass-tomtom/src/io/speed/tomtom_speeds.rs
@@ -17,6 +17,9 @@ where
 {
     let bin_fn = move |c: Cost| -> i64 { (start_time.0 + c.0) / bin_size.0 };
 
+    // https://docs.rs/csv/latest/csv/index.html#example-with-serde
+    // todo: dump each record type into a Vec<Record> so we can look these up
+    // the EdgeId class is also u64, so, the top-level search Vec is indexed by those
     csv::Reader::from_reader(file);
 
     let f: EdgeLookupTable<S> = Box::new(move |(o, e, d, s)| {
@@ -25,6 +28,7 @@ where
         let edge_id = e.edge_id;
         // lookup speed profile by edge id, pick speed by time bin...
         // see https://github.nrel.gov/MBAP/mbap-computing/blob/master/postgres/examples/tomtom_2021_network/tomtom_2021_network_time_bin_speeds.sql
+        // https://pages.github.nrel.gov/MBAP/tomtom_2021_docs/mnr_spec/common_spec/theme_roads_and_ferries/speed_profile/speed_profile.html?hl=speed%2Cprofile
         let speed: CmPerSecond = CmPerSecond(1);
         let tt = e.distance_centimeters.travel_time_millis(&speed);
         Ok(Cost(tt.0))
