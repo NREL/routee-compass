02ce2e06de1915ddf88173e9240980be8222d925
rfitzger <robert.fitzgerald@nrel.gov>
2023-06-13 16:29:55 -0600

    remove io module nesting

--- rust/compass-tomtom/src/io/speed/speed_per_time_slot.rs
+++
@@ -1,9 +0,0 @@
-use serde::Deserialize;
-
-#[derive(Debug, Deserialize)]
-// #[serde(rename_all = "PascalCase")]
-struct SpeedPerTimeSlot {
-    speed_per_time_slot_id: u64,
-    relative_speed: u16,
-    time_slot: u16,
-}
