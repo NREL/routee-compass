a8f01078a5652c2e5b4ea9b05fa6887242dc8b19
rfitzger <robert.fitzgerald@nrel.gov>
2023-06-15 13:05:34 -0600

    log errors reading from read-only memory

--- rust/compass-core/src/algorithm/search/search_error.rs
+++ rust/compass-core/src/algorithm/search/search_error.rs
@@ -1,3 +1,5 @@
+use std::sync::PoisonError;
+
 use crate::model::{
     cost::cost_error::CostError,
     graph::{graph_error::GraphError, vertex_id::VertexId},
@@ -16,6 +18,8 @@ pub enum SearchError {
     CostCalculationError(#[from] CostError),
     #[error("search tree is missing linked vertex {0}")]
     VertexMissingFromSearchTree(VertexId),
+    #[error("error accessing shared read-only dataset: {0}")]
+    ReadOnlyPoisonError(String),
     #[error("internal error due to search logic: {0}")]
     InternalSearchError(String),
 }
