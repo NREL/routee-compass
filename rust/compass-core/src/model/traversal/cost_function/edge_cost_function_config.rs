9952ff0acf0a3232e95805096cbe66722c863d20
rfitzger <robert.fitzgerald@nrel.gov>
2023-07-10 16:23:34 -0600

    share reference to Option contents

--- rust/compass-core/src/model/traversal/cost_function/edge_cost_function_config.rs
+++ rust/compass-core/src/model/traversal/cost_function/edge_cost_function_config.rs
@@ -1,9 +1,9 @@
-use super::function::{EdgeCostFunction, TerminateSearchFunction, ValidFrontierFunction};
+use super::cost_function::{EdgeCostFunction, TerminateSearchFunction, ValidFrontierFunction};
 use crate::model::traversal::state::state_variable::StateVar;
 
 pub struct EdgeCostFunctionConfig<'a> {
     pub cost_fn: &'a EdgeCostFunction,
-    pub valid_fn: &'a Option<ValidFrontierFunction>,
-    pub terminate_fn: &'a Option<TerminateSearchFunction>,
     pub init_state: &'a Vec<StateVar>,
+    pub valid_fn: Option<&'a ValidFrontierFunction>,
+    pub terminate_fn: Option<&'a TerminateSearchFunction>,
 }
--- rust/compass-core/src/model/traversal/cost_function/edge_edge_cost_function_config.rs
+++ rust/compass-core/src/model/traversal/cost_function/edge_edge_cost_function_config.rs
@@ -1,9 +1,9 @@
-use super::function::{EdgeEdgeCostFunction, TerminateSearchFunction, ValidFrontierFunction};
+use super::cost_function::{EdgeEdgeCostFunction, TerminateSearchFunction, ValidFrontierFunction};
 use crate::model::traversal::state::state_variable::StateVar;
 
 pub struct EdgeEdgeCostFunctionConfig<'a> {
     pub cost_fn: &'a EdgeEdgeCostFunction,
-    pub valid_fn: &'a Option<ValidFrontierFunction>,
-    pub terminate_fn: &'a Option<TerminateSearchFunction>,
     pub init_state: &'a Vec<StateVar>,
+    pub valid_fn: Option<&'a ValidFrontierFunction>,
+    pub terminate_fn: Option<&'a TerminateSearchFunction>,
 }
