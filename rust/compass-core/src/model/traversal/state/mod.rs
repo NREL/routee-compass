b5302291410db8c74cf1bc42e8b81541b054ae36
rfitzger <robert.fitzgerald@nrel.gov>
2023-07-10 15:58:10 -0600

    search state types

--- rust/compass-core/src/model/traversal/state/mod.rs
+++ rust/compass-core/src/model/traversal/state/mod.rs
@@ -1,2 +1,3 @@
 pub mod has_travel_time;
+pub mod search_state;
 pub mod state_variable;
---
+++ rust/compass-core/src/model/traversal/state/search_state.rs
@@ -0,0 +1,4 @@
+use super::state_variable::StateVar;
+
+pub type SearchState = Vec<Vec<StateVar>>;
+pub type StateVector = Vec<StateVar>;
