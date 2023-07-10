16a3585e7d80696bc650663c2a8a6393d8fc6188
rfitzger <robert.fitzgerald@nrel.gov>
2023-07-10 14:49:44 -0600

    traversal model v2

--- rust/compass-core/src/model/traversal/mod.rs
+++ rust/compass-core/src/model/traversal/mod.rs
@@ -1,4 +1,9 @@
+pub mod access_result;
+pub mod cost_function;
 pub mod free_flow_traversal_model;
 pub mod state;
+mod tm_v2;
 pub mod traversal_error;
 pub mod traversal_model;
+pub mod traversal_model_config;
+pub mod traversal_result;
