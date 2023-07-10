a337bcab17a3723460d6de9705affea5b5c83b94
rfitzger <robert.fitzgerald@nrel.gov>
2023-07-10 16:23:19 -0600

    public tmv2

--- rust/compass-core/src/model/traversal/mod.rs
+++ rust/compass-core/src/model/traversal/mod.rs
@@ -2,7 +2,7 @@ pub mod access_result;
 pub mod cost_function;
 pub mod free_flow_traversal_model;
 pub mod state;
-mod tm_v2;
+pub mod tm_v2;
 pub mod traversal_error;
 pub mod traversal_model;
 pub mod traversal_model_config;
