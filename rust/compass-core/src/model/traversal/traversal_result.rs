ccd910a9e43e12d2040c3f0fddd59c502927948b
rfitzger <robert.fitzgerald@nrel.gov>
2023-07-11 13:36:00 -0600

    use type alias for SearchState

--- rust/compass-core/src/model/traversal/traversal_result.rs
+++ rust/compass-core/src/model/traversal/traversal_result.rs
@@ -1,8 +1,8 @@
-use super::state::state_variable::StateVar;
 use crate::model::cost::cost::Cost;
+use crate::model::traversal::state::search_state::SearchState;
 
 pub struct TraversalResult {
     pub total_cost: Cost,
     pub cost_vector: Vec<Cost>,
-    pub updated_state: Vec<Vec<StateVar>>,
+    pub updated_state: SearchState,
 }
