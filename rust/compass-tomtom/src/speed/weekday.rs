697afc9431cbf15a548986dc9d4aa9bd45bf9c79
rfitzger <robert.fitzgerald@nrel.gov>
2023-07-14 09:52:58 -0600

    copy/clone required for deref

--- rust/compass-tomtom/src/speed/weekday.rs
+++ rust/compass-tomtom/src/speed/weekday.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Copy, Clone)]
 pub enum Weekday {
     Monday,
     Tuesday,
