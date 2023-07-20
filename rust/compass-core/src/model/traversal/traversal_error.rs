7503a5814c4aa11dc27180e6031ebacbdaead89f
rfitzger <robert.fitzgerald@nrel.gov>
2023-07-20 16:40:54 -0600

    missing id error

--- rust/compass-core/src/model/traversal/traversal_error.rs
+++ rust/compass-core/src/model/traversal/traversal_error.rs
@@ -2,6 +2,8 @@
 pub enum TraversalError {
     #[error("internal error, state variable index is invalid")]
     InvalidStateVariableIndexError,
+    #[error("id {0} for id type {1} not found in tabular edge cost function {2}")]
+    MissingIdInTabularCostFunction(String, String, String),
     #[error("tough stuff brah")]
     Error,
 }
