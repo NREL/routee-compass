1d9d58ac3a05c9deba8e805b9d33172d14e41293
rfitzger <robert.fitzgerald@nrel.gov>
2023-08-01 10:05:54 -0600

    test app w debug output

--- rust/compass-app/src/main.rs
+++ rust/compass-app/src/main.rs
@@ -4,6 +4,7 @@ use compass_app::app::search::search_app::SearchApp;
 use compass_app::config::app_config::AppConfig;
 use compass_app::config::graph::GraphConfig;
 use compass_core::algorithm::search::min_search_tree::a_star::cost_estimate_function::Haversine;
+use compass_core::model::cost::cost::Cost;
 use compass_core::model::traversal::traversal_model::TraversalModel;
 use compass_core::model::units::Velocity;
 use compass_tomtom::graph::{tomtom_graph::TomTomGraph, tomtom_graph_config::TomTomGraphConfig};
@@ -12,6 +13,7 @@ use rand::seq::SliceRandom;
 use std::env;
 use std::error::Error;
 use std::path::PathBuf;
+use std::time::Duration;
 use uom::si::velocity::kilometer_per_hour;
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -25,8 +27,8 @@ fn main() -> Result<(), Box<dyn Error>> {
         )),
         Some(arg) => Ok(arg),
     }?;
-
-    let config = AppConfig::from_path(PathBuf::from(config_file_string))?;
+    let path = PathBuf::from(config_file_string);
+    let config = AppConfig::from_path(&path)?;
 
     let graph = match config.graph {
         GraphConfig::TomTom {
@@ -57,7 +59,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         travel_speed: Velocity::new::<kilometer_per_hour>(40.0),
     };
     let traversal_model: TraversalModel = config.search.traversal_model.try_into()?;
-    let compass_app: SearchApp = SearchApp::new(&graph, &traversal_model, &haversine);
+    let search_app: SearchApp = SearchApp::new(&graph, &traversal_model, &haversine);
 
     let (o, d) = (
         graph
@@ -85,17 +87,26 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let search_start = Local::now();
     log::info!("running search");
-    let results = compass_app.run_edge_oriented(queries)?;
+    let results = search_app.run_edge_oriented(queries)?;
     let search_duration = Local::now() - search_start;
     log::info!("finished search with duration {:?}", search_duration);
 
-    // write each search result to it's own CSV file in the current working directory
+    // (replace this section with output plugins)
     for result in results {
-        let out = PathBuf::from(format!("{}-{}.csv", result.origin, result.destination));
-        let mut writer = csv::Writer::from_path(out)?;
+        let links = result.route.clone().len();
+        let mut time_secs = Cost::ZERO;
         for traversal in result.route {
-            writer.serialize(traversal)?;
+            let cost = traversal.edge_cost();
+            time_secs = time_secs + cost;
         }
+        let dur = Duration::from_secs_f64((time_secs.0).0);
+        log::info!(
+            "{} -> {} had {} links, total time of {:?}",
+            result.origin,
+            result.destination,
+            links,
+            dur
+        );
     }
     return Ok(());
 }
