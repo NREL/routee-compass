42615fe0c4f194f7c4479d60beb04911af49812b
Nicholas Reinicke <Nicholas.Reinicke@nrel.gov>
2023-08-02 13:40:50 -0600

    revert routee random forest

--- rust/compass-powertrain/src/routee/routee_random_forest.rs
+++ rust/compass-powertrain/src/routee/routee_random_forest.rs
@@ -3,9 +3,11 @@ use std::path::PathBuf;
 use compass_core::model::{
     cost::cost::Cost,
     traversal::{
-        function::{cost_function_error::CostFunctionError, function::EdgeCostFunction},
-        state::{search_state::StateVector, state_variable::StateVar},
-        traversal_error::TraversalError,
+        function::{
+            cost_function_error::CostFunctionError,
+            function::EdgeCostFunction
+        },
+        state::{search_state::StateVector, state_variable::StateVar}, traversal_error::TraversalError,
     },
 };
 
@@ -16,13 +18,11 @@ use smartcore::{
 };
 
 use compass_core::model::traversal::function::default::velocity::edge_velocity_lookup::{
-    build_edge_velocity_lookup, initial_velocity_state,
+    build_edge_velocity_lookup, initial_velocity_state
 };
 
-pub fn build_routee_random_forest(
-    routee_model_path: String,
-    speed_table_file: String,
-) -> Result<EdgeCostFunction, CostFunctionError> {
+pub fn build_routee_random_forest(routee_model_path: String, 
+    speed_table_file: String) -> Result<EdgeCostFunction, CostFunctionError> {
     // load the random forest model here, similar to route prototype
     // copy that code over from prototype into this module instead of
     // referencing code directly from the compass_prototype lib
@@ -30,36 +30,33 @@ pub fn build_routee_random_forest(
     ////////////////////////////////////////////////////////////////////////////
     // managing arguments
 
-    // the arguments for routee powertrain include
-    // grade, speed, vehicle parameters, and distance.
+    // the arguments for routee powertrain include 
+    // grade, speed, vehicle parameters, and distance. 
     // these will come from different places:
-
-    // distance and grade come directly from
+    
+    // distance and grade come directly from 
     // the Edge parameter of the EdgeCostFunction
-    // speeds come from a speed lookup table
-    // which must be passed as an argument to
+    // speeds come from a speed lookup table 
+    // which must be passed as an argument to 
     // the routee powertrain EdgeCostFunction builder
-    // a VehicleParameters lookup table needs to be constructed
+    // a VehicleParameters lookup table needs to be constructed 
     // in the RouteE Powertrain EdgeCostFunction builder
-    // an application workflow, when route powertrains are requested,
+    // an application workflow, when route powertrains are requested, 
     // reify these dependencies based on this order:
-
+    
     // takes speeds &EdgeCostFunction argument
     // read in routee powertrain random forest
-    // user submits query including vehicle parameters, used with speeds table
+    // user submits query including vehicle parameters, used with speeds table 
     // to build an instance of routee powertrain edge lookup table
     ////////////////////////////////////////////////////////////////////////////
-
+    
     // Build speed table
     let speed_table = build_edge_velocity_lookup(speed_table_file)?;
     // Load random forest binary file
-    let rf_binary = std::fs::read(routee_model_path.clone())
-        .map_err(|e| CostFunctionError::FileReadError(routee_model_path.clone(), e.to_string()))?;
+    let rf_binary = std::fs::read(routee_model_path.clone()).map_err(|e| CostFunctionError::FileReadError(routee_model_path.clone(), e.to_string()))?;
     let rf: RandomForestRegressor<f64, f64, DenseMatrix<f64>, Vec<f64>> =
-        bincode::deserialize(&rf_binary).map_err(|e| {
-            CostFunctionError::FileReadError(routee_model_path.clone(), e.to_string())
-        })?;
-
+        bincode::deserialize(&rf_binary).map_err(|e| CostFunctionError::FileReadError(routee_model_path.clone(), e.to_string()))?;
+    
     // Build edge function
     let f: EdgeCostFunction = Box::new(move |o, e, d, s| {
         //
@@ -70,15 +67,12 @@ pub fn build_routee_random_forest(
         let grade = e.grade;
         let distance_mile = distance.get::<si::length::mile>();
         let grade_percent = grade.get::<si::ratio::percent>();
-        let speed_kph_float: f64 = speed_kph.into();
-        let speed_mph = speed_kph_float * 0.621371;
+        let speed_mph = speed_kph.into_f64() * 0.621371;
         let x = DenseMatrix::from_2d_vec(&vec![vec![speed_mph, grade_percent]]);
-        let energy_per_mile = rf.predict(&x).map_err(|e| {
-            TraversalError::PredictionModel(routee_model_path.clone(), e.to_string())
-        })?;
+        let energy_per_mile = rf.predict(&x).map_err(|e| TraversalError::PredictionModel(routee_model_path.clone(), e.to_string()))?;
         let energy_cost = energy_per_mile[0] * distance_mile;
         let updated_state = s[0].0 + energy_cost;
-        Ok((Cost::from(energy_cost), vec![StateVar(updated_state)]))
+        Ok((Cost::from_f64(energy_cost), vec![StateVar(updated_state)]))
     });
     return Ok(f);
 }
@@ -87,7 +81,10 @@ pub fn build_routee_random_forest(
 mod tests {
     use crate::routee::routee_random_forest::build_routee_random_forest;
 
-    use compass_core::model::traversal::function::default::velocity::edge_velocity_lookup::initial_velocity_state;
+    use super::build_edge_velocity_lookup;
+    use compass_tomtom::speed::lookup::edge_velocity_lookup::initial_velocity_state;
+    use compass_core::model::cost::cost::Cost;
+    use compass_core::model::traversal::state::state_variable::StateVar;
     use compass_core::model::units::{Length, Ratio};
     use compass_core::model::{
         graph::{edge_id::EdgeId, vertex_id::VertexId},
@@ -105,10 +102,10 @@ mod tests {
             .join("test")
             .join("velocities.txt");
         let model_file_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
-            .join("src")
-            .join("routee")
-            .join("test")
-            .join("Toyota_Camry.bin");
+        .join("src")
+        .join("routee")
+        .join("test")
+        .join("Toyota_Camry.bin");
         let speed_file_name = speed_file_path.to_str().unwrap();
         let model_file_name = model_file_path.to_str().unwrap();
         let v = Vertex {
@@ -125,11 +122,7 @@ mod tests {
                 grade: Ratio::new::<si::ratio::per_mille>(0.0),
             };
         }
-        let rf_predictor = build_routee_random_forest(
-            String::from(model_file_name),
-            String::from(speed_file_name),
-        )
-        .unwrap();
+        let rf_predictor = build_routee_random_forest(String::from(model_file_name), String::from(speed_file_name)).unwrap();
         let initial = initial_velocity_state();
         let e1 = mock_edge(0);
         // 100 meters @ 10kph should take 36 seconds ((0.1/10) * 3600)
