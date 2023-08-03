2b08673e89e1ef06ef5031ed8b6b38d3361de73c
rfitzger <robert.fitzgerald@nrel.gov>
2023-08-03 11:27:46 -0600

    add a generic configuration error type

--- rust/compass-core/src/model/traversal/function/cost_function_error.rs
+++ rust/compass-core/src/model/traversal/function/cost_function_error.rs
@@ -2,6 +2,8 @@ use crate::model::traversal::state::search_state::StateVector;
 
 #[derive(thiserror::Error, Debug, Clone)]
 pub enum CostFunctionError {
+    #[error("failure building cost function from configuration: {0}")]
+    ConfigurationError(String),
     #[error("index {0} for {1} not found on state vector {2:?}")]
     StateVectorIndexOutOfBounds(usize, String, StateVector),
     #[error("failure reading source file {0}: {1}")]
