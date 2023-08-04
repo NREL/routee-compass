13bac4e6f02ef1849224f67697e605bdf20361eb
rfitzger <robert.fitzgerald@nrel.gov>
2023-08-04 15:06:41 -0600

    debugging + display improvement

--- rust/compass-core/src/algorithm/search/min_search_tree/a_star/a_star_traversal.rs
+++ rust/compass-core/src/algorithm/search/min_search_tree/a_star/a_star_traversal.rs
@@ -2,7 +2,7 @@ use std::fmt::Display;
 
 use crate::{algorithm::search::edge_traversal::EdgeTraversal, model::graph::vertex_id::VertexId};
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub struct AStarTraversal {
     pub terminal_vertex: VertexId,
     pub edge_traversal: EdgeTraversal,
@@ -12,7 +12,8 @@ impl Display for AStarTraversal {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(
             f,
-            "-[acost:{} tcost:{} state:{:?}]-> ({})",
+            "-[edge:{} acost:{} tcost:{} state:{:?}]-> ({})",
+            self.edge_traversal.edge_id,
             self.edge_traversal.access_cost,
             self.edge_traversal.traversal_cost,
             self.edge_traversal.result_state,
