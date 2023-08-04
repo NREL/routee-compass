89d657c17b7500c2353873922045ce78d93053ee
rfitzger <robert.fitzgerald@nrel.gov>
2023-08-04 15:50:15 -0600

    add timeunit enum

--- rust/compass-core/src/model/units.rs
+++ rust/compass-core/src/model/units.rs
@@ -1,1 +1,11 @@
+// imports all f64 SI units at the module level
+// like this:
+// use compass_core::model::units::Velocity;
+use serde::Deserialize;
 pub use uom::si::f64::*;
+
+#[derive(Debug, Deserialize, Clone)]
+pub enum TimeUnit {
+    Seconds,
+    Milliseconds,
+}
