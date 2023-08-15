46f7132eac205358b429ca74807c72b62714e4c6
rfitzger <robert.fitzgerald@nrel.gov>
2023-08-15 15:12:26 -0600

    errors from application plugins

--- rust/compass-app/src/app/app_error.rs
+++ rust/compass-app/src/app/app_error.rs
@@ -1,9 +1,13 @@
 use compass_core::algorithm::search::search_error::SearchError;
 
-#[derive(thiserror::Error, Debug, Clone)]
+use crate::plugin::plugin_error::PluginError;
+
+#[derive(thiserror::Error, Debug)]
 pub enum AppError {
     #[error("search failure")]
     SearchError(#[from] SearchError),
+    #[error("application plugin caused a failure")]
+    PluginError(#[from] PluginError),
     #[error("internal error: {0}")]
     InternalError(String),
 }
