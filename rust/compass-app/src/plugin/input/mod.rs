c1503f7ef55b9ea0699fde7e1b4e70864a8469f7
rfitzger <robert.fitzgerald@nrel.gov>
2023-08-18 09:31:21 -0600

    module for applying input plugins

--- rust/compass-app/src/plugin/input/mod.rs
+++ rust/compass-app/src/plugin/input/mod.rs
@@ -2,6 +2,7 @@ use super::plugin_error::PluginError;
 
 pub mod input_field;
 pub mod input_json_extensions;
+pub mod input_plugin_ops;
 pub mod rtree;
 
 pub type InputPlugin = Box<dyn Fn(&serde_json::Value) -> Result<serde_json::Value, PluginError>>;
