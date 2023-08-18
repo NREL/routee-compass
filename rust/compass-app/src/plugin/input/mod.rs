1af9129c585fe423552fb34afef971e8309dcd31
rfitzger <robert.fitzgerald@nrel.gov>
2023-08-18 12:17:29 -0600

    move into compass_app file as helper fn

--- rust/compass-app/src/plugin/input/mod.rs
+++ rust/compass-app/src/plugin/input/mod.rs
@@ -2,7 +2,6 @@ use super::plugin_error::PluginError;
 
 pub mod input_field;
 pub mod input_json_extensions;
-pub mod input_plugin_ops;
 pub mod rtree;
 
 pub type InputPlugin = Box<dyn Fn(&serde_json::Value) -> Result<serde_json::Value, PluginError>>;
