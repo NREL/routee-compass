48965f22663e90efaad97dc5ce50713d1e642dd5
Nicholas Reinicke <Nicholas.Reinicke@nrel.gov>
2023-08-22 13:04:15 -0600

    update road class function to include a set of valid road clases

--- rust/compass-core/src/model/traversal/default/valid_frontier/road_class.rs
+++ rust/compass-core/src/model/traversal/default/valid_frontier/road_class.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 use crate::model::{
     property::{edge::Edge, road_class::RoadClass},
     traversal::state::traversal_state::TraversalState,
@@ -5,9 +7,10 @@ use crate::model::{
 
 use super::ValidFunction;
 
-pub fn build_road_class_valid_fn(max_road_class: RoadClass) -> ValidFunction {
+/// Builds a valid frontier function that checks if the road class of the edge is in the given set.
+pub fn build_road_class_valid_fn(valid_road_classes: HashSet<RoadClass>) -> ValidFunction {
     Box::new(move |edge: &Edge, _state: &TraversalState| {
         let road_class = edge.road_class;
-        Ok(road_class <= max_road_class)
+        Ok(valid_road_classes.contains(&road_class))
     })
 }
