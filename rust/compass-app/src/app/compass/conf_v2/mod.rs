63e8a752bc3114cf35daef01c3e06ced5210b15d
rfitzger <robert.fitzgerald@nrel.gov>
2023-08-22 16:49:14 -0600

    change new config module name

--- rust/compass-app/src/app/compass/conf_v2/mod.rs
+++
@@ -1,4 +0,0 @@
-pub mod compass_app_builder;
-pub mod compass_configuration_error;
-pub mod traversal_model;
-pub mod traversal_model_builder;
--- rust/compass-app/src/app/compass/conf_v2/traversal_model/mod.rs
+++
@@ -1,3 +0,0 @@
-pub mod distance_builder;
-pub mod energy_model_builder;
-pub mod velocity_lookup_builder;
