63447db419a46924c7aec58330a36cd90981b7bc
rfitzger <robert.fitzgerald@nrel.gov>
2023-08-22 15:35:07 -0600

    config-related errors

--- rust/compass-app/src/app/app_error.rs
+++ rust/compass-app/src/app/app_error.rs
@@ -1,11 +1,13 @@
+use super::compass::{
+    compass_input_field::CompassInputField,
+    conf_v2::compass_configuration_error::CompassConfigurationError,
+};
+use crate::plugin::plugin_error::PluginError;
 use compass_core::{
     algorithm::search::search_error::SearchError,
     model::traversal::traversal_model_error::TraversalModelError,
 };
-
-use crate::plugin::plugin_error::PluginError;
-
-use super::compass::compass_input_field::CompassInputField;
+use config::ConfigError;
 
 #[derive(thiserror::Error, Debug)]
 pub enum AppError {
@@ -19,6 +21,10 @@ pub enum AppError {
     IOError(#[from] std::io::Error),
     #[error(transparent)]
     CodecError(#[from] serde_json::Error),
+    #[error(transparent)]
+    ConfigError(#[from] ConfigError),
+    #[error(transparent)]
+    CompassConfigurationError(#[from] CompassConfigurationError),
     #[error("a ux component caused a failure: {0}")]
     UXError(String),
     #[error("internal error: {0}")]
