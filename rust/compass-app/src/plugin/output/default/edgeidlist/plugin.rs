f9338caa418e335f53b5a0c31ac3c196183b920d
rfitzger <robert.fitzgerald@nrel.gov>
2023-08-30 14:56:09 -0600

    implement edge id list plugin

--- rust/compass-app/src/plugin/output/default/edgeidlist/plugin.rs
+++ rust/compass-app/src/plugin/output/default/edgeidlist/plugin.rs
@@ -1,14 +1,8 @@
-use crate::plugin::plugin_error::PluginError;
-
-use compass_core::{
-    algorithm::search::{edge_traversal::EdgeTraversal, search_error::SearchError},
-    model::cost::cost::Cost,
-};
-use serde_json;
-
+use crate::plugin::output::default::edgeidlist::json_extensions::EdgeListJsonExtensions;
 use crate::plugin::output::output_plugin::OutputPlugin;
-
-use super::json_extensions::EdgeListJsonExtensions;
+use crate::plugin::plugin_error::PluginError;
+use compass_core::algorithm::search::{edge_traversal::EdgeTraversal, search_error::SearchError};
+use serde_json;
 
 pub struct EdgeIdListOutputPlugin {}
 
@@ -18,13 +12,14 @@ impl OutputPlugin for EdgeIdListOutputPlugin {
         output: &serde_json::Value,
         search_result: Result<&Vec<EdgeTraversal>, SearchError>,
     ) -> Result<serde_json::Value, PluginError> {
-        todo!()
+        match search_result {
+            Err(_e) => Ok(output.clone()),
+            Ok(r) => {
+                let edge_ids = r.clone().iter().map(|e| e.edge_id).collect::<Vec<_>>();
+                let mut updated = output.clone();
+                updated.add_edge_list(&edge_ids)?;
+                Ok(updated)
+            }
+        }
     }
-}
-
-#[cfg(test)]
-
-mod tests {
-
-    use super::*;
 }
