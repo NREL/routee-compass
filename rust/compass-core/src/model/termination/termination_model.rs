db66f21b0b770772ed7a6370020be7ccaa6c659b
rfitzger <robert.fitzgerald@nrel.gov>
2023-09-01 13:12:16 -0600

    termination models for app

--- rust/compass-core/src/model/termination/termination_model.rs
+++ rust/compass-core/src/model/termination/termination_model.rs
@@ -1,21 +1,45 @@
+use serde::Deserialize;
+use std::time::{Duration, Instant};
+
 use super::termination_model_error::TerminationModelError;
-use crate::model::{property::edge::Edge, traversal::state::traversal_state::TraversalState};
-use std::time::Instant;
 
-pub trait TerminationModel: Send + Sync {
-    fn terminate_search(
-        &self,
-        edge: &Edge,
-        state: &TraversalState,
-        start_time: &Instant,
-        iterations: u64,
-    ) -> Result<bool, TerminationModelError>;
+#[derive(Debug, Deserialize)]
+pub enum TerminationModel {
+    #[serde(rename = "query_runtime")]
+    QueryRuntimeLimit { limit: Duration, frequency: u64 },
+    #[serde(rename = "solution_size")]
+    SolutionSizeLimit { limit: usize },
+    #[serde(rename = "iterations")]
+    IterationsLimit { limit: u64 },
+    #[serde(rename = "combined")]
+    Combined { models: Vec<TerminationModel> },
+}
 
-    fn summarize_termination(
+impl TerminationModel {
+    /// predicate to test whether a query should terminate based on
+    /// application-level configurations
+    pub fn terminate_search(
         &self,
-        edge: &Edge,
-        state: &TraversalState,
         start_time: &Instant,
+        solution_size: usize,
         iterations: u64,
-    ) -> Result<Option<String>, TerminationModelError>;
+    ) -> Result<bool, TerminationModelError> {
+        use TerminationModel as T;
+        match self {
+            T::QueryRuntimeLimit { limit, frequency } => {
+                if iterations % frequency == 0 {
+                    let dur = Instant::now().duration_since(*start_time);
+                    Ok(dur < *limit)
+                } else {
+                    Ok(false)
+                }
+            }
+            T::SolutionSizeLimit { limit } => Ok(solution_size < *limit),
+            T::IterationsLimit { limit } => Ok(iterations < *limit),
+            T::Combined { models } => models.iter().try_fold(false, |acc, m| {
+                m.terminate_search(start_time, solution_size, iterations)
+                    .map(|r| acc && r)
+            }),
+        }
+    }
 }
