787cbe557098141e2e27d4387b84948784b98a63
rfitzger <robert.fitzgerald@nrel.gov>
2023-09-12 13:07:01 -0600

    tree output plugin option

--- rust/compass-app/src/plugin/output/default/geometry/builder.rs
+++ rust/compass-app/src/plugin/output/default/geometry/builder.rs
@@ -15,6 +15,8 @@ impl OutputPluginBuilder for GeometryPluginBuilder {
         parameters: &serde_json::Value,
     ) -> Result<Box<dyn OutputPlugin>, CompassConfigurationError> {
         let geometry_filename_key = String::from("edge_file");
+        let route_geometry_key = String::from("route_geometry");
+        let tree_geometry_key = String::from("tree_geometry");
         let geometry_filename = parameters
             .get(&geometry_filename_key)
             .ok_or(CompassConfigurationError::ExpectedFieldForComponent(
@@ -27,8 +29,31 @@ impl OutputPluginBuilder for GeometryPluginBuilder {
                 geometry_filename_key.clone(),
                 String::from("String"),
             ))?;
-        let geom_plugin = GeometryPlugin::from_file(&geometry_filename)
-            .map_err(CompassConfigurationError::PluginError)?;
+        let route_geometry = parameters
+            .get(&route_geometry_key)
+            .ok_or(CompassConfigurationError::ExpectedFieldForComponent(
+                route_geometry_key.clone(),
+                String::from("Geometry Output Plugin"),
+            ))?
+            .as_bool()
+            .ok_or(CompassConfigurationError::ExpectedFieldWithType(
+                route_geometry_key.clone(),
+                String::from("String"),
+            ))?;
+        let tree_geometry = parameters
+            .get(&tree_geometry_key)
+            .ok_or(CompassConfigurationError::ExpectedFieldForComponent(
+                tree_geometry_key.clone(),
+                String::from("Geometry Output Plugin"),
+            ))?
+            .as_bool()
+            .ok_or(CompassConfigurationError::ExpectedFieldWithType(
+                tree_geometry_key.clone(),
+                String::from("String"),
+            ))?;
+        let geom_plugin =
+            GeometryPlugin::from_file(&geometry_filename, route_geometry, tree_geometry)
+                .map_err(CompassConfigurationError::PluginError)?;
         Ok(Box::new(geom_plugin))
     }
 }
