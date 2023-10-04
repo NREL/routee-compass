574d5d0c1754399d3d604911cf912c4757b2c18e
rfitzger <robert.fitzgerald@nrel.gov>
2023-10-04 10:57:02 -0600

    eq for EnergyUnit

--- rust/compass-core/src/util/unit/energy_unit.rs
+++ rust/compass-core/src/util/unit/energy_unit.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Serialize, Deserialize, Clone)]
+#[derive(Debug, Serialize, Deserialize, Clone, PartialEq)]
 #[serde(rename_all = "snake_case")]
 pub enum EnergyUnit {
     GallonsGasoline,
