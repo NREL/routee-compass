c8c3d5575f7e49bac5d9f44b87ab81ead2be6549
rfitzger <robert.fitzgerald@nrel.gov>
2023-10-04 10:58:07 -0600

    ideal energy rate param, better units handling

--- rust/compass-powertrain/src/routee/speed_grade_model_service.rs
+++ rust/compass-powertrain/src/routee/speed_grade_model_service.rs
@@ -21,7 +21,8 @@ pub struct SpeedGradeModelService {
     pub energy_model_grade_unit: GradeUnit,
     pub graph_grade_unit: GradeUnit,
     pub output_time_unit: TimeUnit,
-    pub minimum_energy_rate: EnergyRate,
+    pub output_distance_unit: DistanceUnit,
+    pub ideal_energy_rate: EnergyRate,
 }
 
 impl SpeedGradeModelService {
@@ -30,13 +31,16 @@ impl SpeedGradeModelService {
         speeds_table_speed_unit: SpeedUnit,
         energy_model_path: String,
         model_type: ModelType,
+        ideal_energy_rate_option: Option<EnergyRate>,
         energy_model_speed_unit: SpeedUnit,
         energy_model_grade_unit: GradeUnit,
         graph_grade_unit: GradeUnit,
         energy_model_energy_rate_unit: EnergyRateUnit,
         output_time_unit_option: Option<TimeUnit>,
+        output_distance_unit_option: Option<DistanceUnit>,
     ) -> Result<Self, TraversalModelError> {
         let output_time_unit = output_time_unit_option.unwrap_or(BASE_TIME_UNIT);
+        let output_distance_unit = output_distance_unit_option.unwrap_or(BASE_DISTANCE_UNIT);
 
         let energy_model = model_type.build(
             energy_model_path.clone(),
@@ -45,10 +49,10 @@ impl SpeedGradeModelService {
             energy_model_energy_rate_unit.clone(),
         )?;
 
-        let minimum_energy_rate = find_min_energy_rate(
-            &energy_model,
-            &energy_model_energy_rate_unit,
-        )?;
+        let ideal_energy_rate = match ideal_energy_rate_option {
+            None => find_min_energy_rate(&energy_model, &energy_model_energy_rate_unit)?,
+            Some(ier) => ier,
+        };
 
         // load speeds table
         let speed_table: Arc<Vec<Speed>> = Arc::new(
@@ -91,7 +95,8 @@ impl SpeedGradeModelService {
             energy_model_grade_unit,
             graph_grade_unit,
             output_time_unit,
-            minimum_energy_rate,
+            output_distance_unit,
+            ideal_energy_rate,
         })
     }
 }
@@ -109,12 +114,7 @@ pub fn find_min_energy_rate(
     for speed_i32 in 20..80 {
         let speed = Speed::new(speed_i32 as f64);
         let (energy_rate, _) = model
-            .predict(
-                speed,
-                SpeedUnit::MilesPerHour,
-                grade,
-                GradeUnit::Percent,
-            )
+            .predict(speed, SpeedUnit::MilesPerHour, grade, GradeUnit::Percent)
             .map_err(|e| TraversalModelError::PredictionModel(e.to_string()))?;
         if energy_rate < minimum_energy_rate {
             minimum_energy_rate = energy_rate;
