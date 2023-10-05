3af05d0ffa93ff9ae96aeab90a73c2e244d32603
Nicholas Reinicke <Nicholas.Reinicke@nrel.gov>
2023-10-05 15:21:31 -0600

    only initialize rayon pool once

--- rust/compass-app/src/app/compass/compass_app.rs
+++ rust/compass-app/src/app/compass/compass_app.rs
@@ -123,6 +123,12 @@ impl TryFrom<(&Config, &CompassAppBuilder)> for CompassApp {
         // build search app
         let search_app_start = Local::now();
         let parallelism = config.get::<usize>(CompassConfigurationField::Parallelism.to_str())?;
+        let pool = rayon::ThreadPoolBuilder::new()
+            .num_threads(parallelism)
+            .build_global()
+            .map_err(|e| {
+                AppError::InternalError(format!("failure getting thread pool: {}", e.to_string()))
+            })?;
         let search_app: SearchApp = SearchApp::new(
             search_algorithm,
             graph,
@@ -185,15 +191,7 @@ impl CompassApp {
         let input_queries: Vec<serde_json::Value> = input_bundles.into_iter().flatten().collect();
 
         // run parallel searches using a rayon thread pool
-        let pool = rayon::ThreadPoolBuilder::new()
-            .num_threads(self.parallelism)
-            .build_global()
-            .map_err(|e| {
-                AppError::InternalError(format!("failure getting thread pool: {}", e.to_string()))
-            })?;
-
         log::info!("using {} threads to run queries", current_num_threads());
-
         let run_query_result: Vec<serde_json::Value> = input_queries
             .into_par_iter()
             .map(|query| self.run_single_query(query))
