d87aeef32ac40a30c0371f0c9cbd7cd6bd40065f
rfitzger <robert.fitzgerald@nrel.gov>
2023-10-09 14:21:56 -0600

    logging, distance default

--- python/nrel/routee/compass/io.py
+++ python/nrel/routee/compass/io.py
@@ -22,7 +22,9 @@ def generate_compass_dataset(
         output_directory: Path,
         hwy_speeds: Optional[Dict] = None,
         fallback: Optional[float] = None,
-        agg: Callable = np.mean
+        agg: Callable = np.mean,
+        default_config: bool = True,
+        default_energy_model: bool = True
         ):
     """processes a graph downloaded via OSMNx, generating the set of input
     files required for running RouteE Compass.
@@ -39,33 +41,40 @@ def generate_compass_dataset(
     :type g: MultiDiGraph
     :param output_directory: directory path to use for writing new Compass files
     :type output_directory: Path
-    :param hwy_speeds OSM highway types and values = typical speeds (km per
+    :param hwy_speeds: OSM highway types and values = typical speeds (km per
                       hour) to assign to edges of that highway type for any edges missing
                       speed data. Any edges with highway type not in `hwy_speeds` will be
                       assigned the mean preexisting speed value of all edges of that highway
                       type.
     :type hwy_speeds: Optional[Dict]
-    :param fallback default speed value (km per hour) to assign to edges whose highway
-                    type did not appear in `hwy_speeds` and had no preexisting speed
-                    values on any edge
+    :param fallback: default speed value (km per hour) to assign to edges whose highway
+                     type did not appear in `hwy_speeds` and had no preexisting speed
+                     values on any edge
     :type fallback: Optional[float]
     :param agg: aggregation function to impute missing values from observed values.
                 the default is numpy.mean, but you might also consider for example
                 numpy.median, numpy.nanmedian, or your own custom function
     :type agg: Callable
+    :param default_config: if true, copy default configuration files into the output directory
+    :type default_config: bool
+    :param default_energy_model: if true, copy a trained RouteE Powertrain model into the output directory
+    :type default_energy_model: bool
     """
     
     # pre-process the graph
+    log.info("processing graph topology and speeds")
     g1 = g.copy()
     g1 = ox.utils_graph.get_largest_component(g1)
     g1 = ox.add_edge_speeds(g1, hwy_speeds=hwy_speeds, fallback=fallback, agg=agg)
     v, e = ox.graph_to_gdfs(g1)
 
     # process vertices
+    log.info("processing vertices")
     v = v.reset_index(drop=False).rename(columns={"osmid": "vertex_uuid"})
     v['vertex_id'] = range(len(v))
 
     # process edges
+    log.info("processing edges")
     lookup = v.set_index("vertex_uuid")
     def replace_id(vertex_uuid):
         return lookup.loc[vertex_uuid].vertex_id
@@ -80,35 +89,42 @@ def generate_compass_dataset(
     e['src_vertex_id'] = e.src_vertex_uuid.apply(replace_id)
     e['dst_vertex_id'] = e.dst_vertex_uuid.apply(replace_id)
     
-    # NETWORK FILES
+    # WRITE NETWORK FILES
     output_directory.mkdir(parents=True, exist_ok=True)
     #   vertex tables
+    log.info("writing vertex files")
     v.to_csv(output_directory / "vertices-complete.csv.gz", index=False)
     v[['vertex_id', 'vertex_uuid']].to_csv(output_directory / "vertices-mapping.csv.gz", index=False)
     v[['vertex_uuid']].to_csv(output_directory / "vertices-uuid-enumerated.txt.gz", index=False, header=False)
     v[['vertex_id', 'x', 'y']].to_csv(output_directory / "vertices-compass.csv.gz", index=False)
 
     #   edge tables (CSV)
+    log.info("writing edge files")
     compass_cols = ['edge_id', 'src_vertex_id', 'dst_vertex_id', 'distance']
     e.to_csv(output_directory / "edges-complete.csv.gz", index=False)
     e[compass_cols].to_csv(output_directory / "edges-compass.csv.gz", index=False)
     e[['edge_id', 'edge_uuid']].to_csv(output_directory / "edges-mapping.csv.gz", index=False)
 
     #   edge tables (TXT)
+    log.info("writing edge attribute files")
     e.edge_uuid.to_csv(output_directory / "edges-uuid-enumerated.txt.gz", index=False, header=False)
     np.savetxt(output_directory / "edges-geometries-enumerated.txt.gz", e.geometry, fmt = "%s") # doesn't quote LINESTRINGS
     e.speed_kph.to_csv(output_directory / "edges-posted-speed-enumerated.txt.gz", index=False, header=False)
     e.highway.to_csv(output_directory / "edges-road-class-enumerated.txt.gz", index=False, header=False)
 
-    # DEFAULT CONFIGURATION FILES
-    for filename in ['osm_default_speed.toml', 'osm_default_energy.toml']:
-        init_toml_file = resource_filename("nrel.routee.compass.resources", filename)
-        with open(init_toml_file, 'r') as f:
-            init_toml = toml.loads(f.read())
-        with open(output_directory / filename, 'w') as f:
-            f.write(toml.dumps(init_toml))
+    # COPY DEFAULT CONFIGURATION FILES
+    if default_config:
+        log.info("copying default configuration TOML files")
+        for filename in ['osm_default_distance.toml', 'osm_default_speed.toml', 'osm_default_energy.toml']:
+            init_toml_file = resource_filename("nrel.routee.compass.resources", filename)
+            with open(init_toml_file, 'r') as f:
+                init_toml = toml.loads(f.read())
+            with open(output_directory / filename, 'w') as f:
+                f.write(toml.dumps(init_toml))
     
-    # ROUTEE ENERGY MODEL
-    camry_src = Path(resource_filename("nrel.routee.compass.resources", "2016_TOYOTA_Camry_4cyl_2WD.bin"))
-    camry_dst = output_directory / "2016_TOYOTA_Camry_4cyl_2WD.bin"
-    shutil.copy(camry_src, camry_dst)
+    # COPY ROUTEE ENERGY MODEL
+    if default_energy_model:
+        log.info("copying example RouteE Powertrain model")
+        camry_src = Path(resource_filename("nrel.routee.compass.resources", "2016_TOYOTA_Camry_4cyl_2WD.bin"))
+        camry_dst = output_directory / "2016_TOYOTA_Camry_4cyl_2WD.bin"
+        shutil.copy(camry_src, camry_dst)
