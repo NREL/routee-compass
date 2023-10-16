093d612577ee7602364bb421ae2df59c8f3bb99f
rfitzger <robert.fitzgerald@nrel.gov>
2023-10-16 12:37:21 -0600

    easier to use as_object_mut instance for add fns

--- rust/compass-app/src/plugin/output/default/traversal/json_extensions.rs
+++ rust/compass-app/src/plugin/output/default/traversal/json_extensions.rs
@@ -1,6 +1,3 @@
-use geo::{LineString, MultiLineString};
-use wkt::ToWkt;
-
 use crate::plugin::plugin_error::PluginError;
 
 pub enum TraversalJsonField {
@@ -22,40 +19,10 @@ impl TraversalJsonField {
 }
 
 pub trait TraversalJsonExtensions {
-    fn add_route_geometry(&mut self, geometry: LineString) -> Result<(), PluginError>;
-    fn add_tree_geometry(&mut self, geometry: MultiLineString) -> Result<(), PluginError>;
     fn get_route_geometry_wkt(&self) -> Result<String, PluginError>;
 }
 
 impl TraversalJsonExtensions for serde_json::Value {
-    fn add_route_geometry(&mut self, geometry: LineString) -> Result<(), PluginError> {
-        let wkt = geometry.wkt_string();
-        match self {
-            serde_json::Value::Object(map) => {
-                let json_string = serde_json::Value::String(wkt);
-                map.insert(TraversalJsonField::RouteOutput.to_string(), json_string);
-                Ok(())
-            }
-            _ => Err(PluginError::InputError(String::from(
-                "OutputResult is not a JSON object",
-            ))),
-        }
-    }
-
-    fn add_tree_geometry(&mut self, geometry: MultiLineString) -> Result<(), PluginError> {
-        let wkt = geometry.wkt_string();
-        match self {
-            serde_json::Value::Object(map) => {
-                let json_string = serde_json::Value::String(wkt);
-                map.insert(TraversalJsonField::TreeOutput.to_string(), json_string);
-                Ok(())
-            }
-            _ => Err(PluginError::InputError(String::from(
-                "OutputResult is not a JSON object",
-            ))),
-        }
-    }
-
     fn get_route_geometry_wkt(&self) -> Result<String, PluginError> {
         let geometry = self
             .get(TraversalJsonField::RouteOutput.as_str())
