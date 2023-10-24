8f8e236fed7b456a6e5d54e232c0f54d80e9582c
rfitzger <robert.fitzgerald@nrel.gov>
2023-10-24 16:11:50 -0600

    docstring

--- rust/compass-core/src/model/traversal/traversal_result.rs
+++ rust/compass-core/src/model/traversal/traversal_result.rs
@@ -1,6 +1,8 @@
 use crate::model::cost::cost::Cost;
 use crate::model::traversal::state::traversal_state::TraversalState;
 
+/// The state of a search after completing an edge traversal, along
+/// with the cost of traversing that edge.
 pub struct TraversalResult {
     pub total_cost: Cost,
     pub updated_state: TraversalState,
