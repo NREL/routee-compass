6822c413480a5c6838d7a016276edf89978e5d47
rfitzger <robert.fitzgerald@nrel.gov>
2023-12-04 08:58:38 -0700

    distance unit for default value

--- rust/routee-compass/src/plugin/input/default/load_balancer/weight_heuristic.rs
+++ rust/routee-compass/src/plugin/input/default/load_balancer/weight_heuristic.rs
@@ -1,9 +1,12 @@
 use crate::plugin::{input::input_json_extensions::InputJsonExtensions, plugin_error::PluginError};
-use routee_compass_core::util::{geo::haversine, unit::as_f64::AsF64};
+use routee_compass_core::util::{
+    geo::haversine,
+    unit::{as_f64::AsF64, Distance, DistanceUnit},
+};
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize, Clone, Copy)]
-#[serde(rename_all = "snake_case")]
+#[serde(rename_all = "snake_case", tag = "type")]
 pub enum WeightHeuristic {
     /// computes a weight directly as the haversine distance estimation between
     /// trip origin and destination, in meters.
@@ -11,17 +14,21 @@ pub enum WeightHeuristic {
     /// # Arguments
     ///
     /// * `default` - fill value if no destination is provided
-    Haversine { default: f64 },
+    Haversine { default: (Distance, DistanceUnit) },
 }
 
 impl WeightHeuristic {
     pub fn estimate_weight(&self, query: serde_json::Value) -> Result<f64, PluginError> {
         match self {
             WeightHeuristic::Haversine { default } => {
+                let (default_distance, distance_unit) = default;
+                let default_meters = DistanceUnit::Meters
+                    .convert(*default_distance, *distance_unit)
+                    .as_f64();
                 let o = query.get_origin_coordinate()?;
                 let d_option = query.get_destination_coordinate()?;
                 match d_option {
-                    None => Ok(*default),
+                    None => Ok(default_meters),
                     Some(d) => haversine::coord_distance_meters(o, d)
                         .map(|d| d.as_f64())
                         .map_err(|s| {
