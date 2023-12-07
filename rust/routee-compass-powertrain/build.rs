58ed2a8839e8f3faf659eb49426d972416905201
Nicholas Reinicke <Nicholas.Reinicke@nrel.gov>
2023-12-06 17:06:22 -0700

    check in work on adding checksums

--- rust/routee-compass-powertrain/build.rs
+++ rust/routee-compass-powertrain/build.rs
@@ -3,8 +3,31 @@ use std::{
     path::{Path, PathBuf},
 };
 
-#[cfg(feature = "onnx")]
+use hex_literal::hex;
+
 const ORT_EXTRACT_DIR: &str = "onnx-runtime";
+const LINUX_X64_CHECKSUM: [u8; 32] =
+    hex!("aac5f22695168f089af0bfd129b5ac2bad86a3cfaba0457a536e21f30f0c155a");
+const LINUX_AARCH64_CHECKSUM: [u8; 32] =
+    hex!("7dded316f8a80c7bf5f91a0c9a4ab8ce854530c8ece40828a448c06e7e8fc453");
+const MACOS_UNIVERSAL2_CHECKSUM: [u8; 32] =
+    hex!("ecb7651c216fe6ffaf4c578e135d98341bc5bc944c5dc6b725ef85b0d7747be0");
+const WINDOWS_X64_CHECKSUM: [u8; 32] =
+    hex!("07c58b7842e288caa7610b16a9feac13e16b4d5a2f0e938de064b3cc83928107");
+
+#[cfg(feature = "onnx")]
+fn check_checksum(buf: &[u8], file_hash: &[u8]) {
+    use sha2::{Digest, Sha256};
+
+    let mut hasher = Sha256::new();
+    hasher.update(buf);
+
+    let hash_result = hasher.finalize();
+
+    if hash_result[..] != file_hash[..] {
+        panic!("Expected file hash does not match the downloaded file")
+    }
+}
 
 #[cfg(feature = "onnx")]
 fn fetch_file(source_url: &str) -> Vec<u8> {
@@ -48,18 +71,21 @@ fn main() {
         return;
     }
 
-    let (archive_type, url) = match env::var("TARGET").unwrap().as_str() {
-        "x86_64-apple-darwin" => (ArchiveType::Tgz, "https://github.com/supertone-inc/onnxruntime-build/releases/download/v1.15.1/onnxruntime-osx-universal2-static_lib-1.15.1.tgz"),
-        "x86_64-unknown-linux-gnu" => (ArchiveType::Tgz, "https://github.com/supertone-inc/onnxruntime-build/releases/download/v1.15.1/onnxruntime-linux-x64-static_lib-1.15.1.tgz"),
-        "aarch64-unknown-linux-gnu" => (ArchiveType::Tgz, "https://github.com/supertone-inc/onnxruntime-build/releases/download/v1.15.1/onnxruntime-linux-aarch64-static_lib-1.15.1.tgz"),
-        "x86_64-pc-windows-msvc" => (ArchiveType::Zip,"https://github.com/supertone-inc/onnxruntime-build/releases/download/v1.15.1/onnxruntime-win-x64-static_lib-1.15.1.zip"),
-        "x86_64-pc-windows-gnu" => (ArchiveType::Zip, "https://github.com/supertone-inc/onnxruntime-build/releases/download/v1.15.1/onnxruntime-win-x64-static_lib-1.15.1.zip"),
+    let (archive_type, url, file_hash) = match env::var("TARGET").unwrap().as_str() {
+        "x86_64-apple-darwin" => (ArchiveType::Tgz, "https://github.com/supertone-inc/onnxruntime-build/releases/download/v1.15.1/onnxruntime-osx-universal2-static_lib-1.15.1.tgz", MACOS_UNIVERSAL2_CHECKSUM),
+        "x86_64-unknown-linux-gnu" => (ArchiveType::Tgz, "https://github.com/supertone-inc/onnxruntime-build/releases/download/v1.15.1/onnxruntime-linux-x64-static_lib-1.15.1.tgz", LINUX_X64_CHECKSUM),
+        "aarch64-unknown-linux-gnu" => (ArchiveType::Tgz, "https://github.com/supertone-inc/onnxruntime-build/releases/download/v1.15.1/onnxruntime-linux-aarch64-static_lib-1.15.1.tgz", LINUX_AARCH64_CHECKSUM),
+        "x86_64-pc-windows-msvc" => (ArchiveType::Zip,"https://github.com/supertone-inc/onnxruntime-build/releases/download/v1.15.1/onnxruntime-win-x64-static_lib-1.15.1.zip", WINDOWS_X64_CHECKSUM),
+        "x86_64-pc-windows-gnu" => (ArchiveType::Zip, "https://github.com/supertone-inc/onnxruntime-build/releases/download/v1.15.1/onnxruntime-win-x64-static_lib-1.15.1.zip", WINDOWS_X64_CHECKSUM),
         t => panic!("Unsupported target:  {t}"),
     };
 
     let out_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join(ORT_EXTRACT_DIR);
     if !out_dir.exists() {
         let buf = fetch_file(url);
+
+        check_checksum(buf.as_slice(), &file_hash);
+
         match archive_type {
             ArchiveType::Tgz => extract_tgz(&buf, &out_dir),
             ArchiveType::Zip => extract_zip(&buf, &out_dir),
