eba34718bb6b70ef3f490e06c3178d0678c6fab4
rfitzger <robert.fitzgerald@nrel.gov>
2023-12-11 13:41:59 -0700

    cleanup

--- rust/routee-compass/src/app/compass/config/frontier_model/road_class/road_class_builder.rs
+++ rust/routee-compass/src/app/compass/config/frontier_model/road_class/road_class_builder.rs
@@ -21,7 +21,6 @@ impl FrontierModelBuilder for RoadClassBuilder {
     ) -> Result<Arc<dyn FrontierModelService>, FrontierModelError> {
         let frontier_key = CompassConfigurationField::Frontier.to_string();
         let road_class_file_key = String::from("road_class_input_file");
-        // let valid_road_class_key = String::from("valid_road_classes");
 
         let road_class_file = parameters
             .get_config_path(road_class_file_key.clone(), frontier_key.clone())
@@ -33,19 +32,6 @@ impl FrontierModelBuilder for RoadClassBuilder {
                 ))
             })?;
 
-        // let road_classes_vec = parameters
-        //     .get_config_serde::<Vec<String>>(valid_road_class_key.clone(), frontier_key.clone())
-        //     .map_err(|e| {
-        //         FrontierModelError::BuildError(format!(
-        //             "configuration error due to {}: {}",
-        //             valid_road_class_key.clone(),
-        //             e
-        //         ))
-        //     })?;
-        // let road_classes: HashSet<String> = HashSet::from_iter(road_classes_vec.to_vec());
-
-        // log::debug!("valid road classes (raw/hashset): {:?}", road_classes_vec);
-
         let road_class_lookup: Box<[String]> =
             read_utils::read_raw_file(road_class_file.clone(), read_decoders::string, None)
                 .map_err(|e| {
@@ -55,16 +41,6 @@ impl FrontierModelBuilder for RoadClassBuilder {
                         e
                     ))
                 })?;
-        // .iter()
-        // .map(|rc| road_classes.contains(rc.trim()))
-        // .collect();
-
-        // let n_good = road_class_lookup.iter().filter(|r| **r).count();
-        // log::debug!(
-        //     "{}/{} links have a valid road class",
-        //     n_good,
-        //     road_class_lookup.len()
-        // );
 
         let m: Arc<dyn FrontierModelService> = Arc::new(RoadClassFrontierService {
             road_class_lookup: Arc::new(road_class_lookup),
