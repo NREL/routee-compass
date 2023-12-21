612852ea660e9be58ea48eba631d2446b4bbfd55
rfitzger <robert.fitzgerald@nrel.gov>
2023-12-21 11:25:57 -0700

    programatically collect state variable names

--- rust/routee-compass/src/app/compass/config/utility_model/cost_model_builder.rs
+++ rust/routee-compass/src/app/compass/config/utility_model/cost_model_builder.rs
@@ -1,17 +1,14 @@
-use std::collections::{HashMap, HashSet};
-
-use routee_compass_core::model::cost::{
-    cost_aggregation::CostAggregation, network::network_cost_mapping::NetworkCostMapping,
-    vehicle::vehicle_cost_mapping::VehicleCostMapping,
-};
-
+use super::cost_model_service::CostModelService;
 use crate::app::compass::config::{
     compass_configuration_error::CompassConfigurationError,
     compass_configuration_field::CompassConfigurationField,
     config_json_extension::ConfigJsonExtensions,
 };
-
-use super::cost_model_service::CostModelService;
+use routee_compass_core::model::cost::{
+    cost_aggregation::CostAggregation, network::network_cost_mapping::NetworkCostMapping,
+    vehicle::vehicle_cost_mapping::VehicleCostMapping,
+};
+use std::collections::{HashMap, HashSet};
 
 pub struct CostModelBuilder {}
 
@@ -25,8 +22,23 @@ impl CostModelBuilder {
             config.get_config_serde_optional(&"vehicle_mapping", &parent_key)?;
         let network_mapping: Option<HashMap<String, NetworkCostMapping>> =
             config.get_config_serde_optional(&"network_mapping", &parent_key)?;
+
+        // collect the complete set of potential state variable names from the keys of these mapping collections
         let default_state_variable_names: Option<HashSet<String>> =
-            config.get_config_serde_optional(&"default_state_variable_names", &parent_key)?;
+            match (&vehicle_mapping, &network_mapping) {
+                (None, None) => None,
+                (None, Some(nm)) => Some(nm.keys().cloned().collect::<HashSet<_>>()),
+                (Some(vm), None) => Some(vm.keys().cloned().collect::<HashSet<_>>()),
+                (Some(vm), Some(nm)) => {
+                    let key_set = vm
+                        .keys()
+                        .cloned()
+                        .chain(nm.keys().cloned())
+                        .collect::<HashSet<_>>();
+                    Some(key_set)
+                }
+            };
+
         let default_cost_aggregation: Option<CostAggregation> =
             config.get_config_serde_optional(&"default_cost_aggregation", &parent_key)?;
         let model = CostModelService::new(
