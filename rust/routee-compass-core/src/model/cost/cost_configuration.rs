4b19228221edfa47aa4628ba3125e3ddac3db0d7
rfitzger <robert.fitzgerald@nrel.gov>
2023-12-22 11:35:21 -0700

    remove stub

--- rust/routee-compass-core/src/model/cost/cost_configuration.rs
+++
@@ -1,9 +0,0 @@
-use super::vehicle::vehicle_cost_mapping::VehicleCostMapping;
-
-pub struct CostConfiguration {
-    distance_cost: Option<VehicleCostMapping>,
-    time_cost: Option<VehicleCostMapping>,
-    electric_cost: Option<VehicleCostMapping>,
-    gas_cost: Option<VehicleCostMapping>,
-    diesel_cost: Option<VehicleCostMapping>,
-}
