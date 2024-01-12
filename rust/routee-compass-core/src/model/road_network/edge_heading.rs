2324880599ae4c7033a9a7ba4406e7f314495cbe
Nicholas Reinicke <Nicholas.Reinicke@nrel.gov>
2024-01-12 16:56:03 -0700

    add edge headings to osm dataset builder

--- rust/routee-compass-core/src/model/road_network/edge_heading.rs
+++ rust/routee-compass-core/src/model/road_network/edge_heading.rs
@@ -2,19 +2,45 @@ use serde::Deserialize;
 
 #[derive(Copy, Clone, Deserialize)]
 pub struct EdgeHeading {
-    pub start_heading: i16,
-    pub end_heading: i16,
+    start_heading: i16,
+    end_heading: Option<i16>,
 }
 
 impl EdgeHeading {
-    /// Compute the angle between this edge and the next edge
-    pub fn next_edge_angle(&self, next_edge_heading: &EdgeHeading) -> i16 {
-        (next_edge_heading.start_heading - self.end_heading + 180) % 360 - 180
+    pub fn with_start_and_end(start_heading: i16, end_heading: i16) -> Self {
+        Self {
+            start_heading,
+            end_heading: Some(end_heading),
+        }
     }
 
-    /// Compute the angle between this edge and the previous edge
-    pub fn previous_edge_angle(&self, previous_edge_heading: &EdgeHeading) -> i16 {
-        (self.start_heading - previous_edge_heading.end_heading + 180) % 360 - 180
+    pub fn with_start(start_heading: i16) -> Self {
+        Self {
+            start_heading,
+            end_heading: None,
+        }
+    }
+
+    pub fn start_heading(&self) -> i16 {
+        self.start_heading
+    }
+    /// If the end heading is not specified, it is assumed to be the same as the start heading
+    pub fn end_heading(&self) -> i16 {
+        match self.end_heading {
+            Some(end_heading) => end_heading,
+            None => self.start_heading,
+        }
+    }
+    /// Compute the angle between this edge and the next edge
+    pub fn next_edge_angle(&self, next_edge_heading: &EdgeHeading) -> i16 {
+        let angle = next_edge_heading.start_heading() - self.end_heading();
+        if angle > 180 {
+            angle - 360
+        } else if angle < -180 {
+            angle + 360
+        } else {
+            angle
+        }
     }
 }
 
@@ -24,30 +50,19 @@ mod test {
 
     #[test]
     fn test_next_edge_angle() {
-        let edge_heading = EdgeHeading {
-            start_heading: 45,
-            end_heading: 90,
-        };
-        let next_edge_heading = EdgeHeading {
-            start_heading: 90,
-            end_heading: 135,
-        };
+        let edge_heading = EdgeHeading::with_start_and_end(45, 90);
+        let next_edge_heading = EdgeHeading::with_start_and_end(90, 135);
         assert_eq!(edge_heading.next_edge_angle(&next_edge_heading), 0);
     }
 
     #[test]
-    fn test_previous_edge_angle() {
-        let edge_heading = EdgeHeading {
-            start_heading: 45,
-            end_heading: 90,
-        };
-        let previous_edge_heading = EdgeHeading {
-            start_heading: 90,
-            end_heading: 135,
-        };
-        assert_eq!(
-            edge_heading.previous_edge_angle(&previous_edge_heading),
-            -90
-        );
+    fn test_next_edge_angle_wrap() {
+        let edge_heading = EdgeHeading::with_start_and_end(10, 10);
+        let next_edge_heading = EdgeHeading::with_start_and_end(350, 350);
+        assert_eq!(edge_heading.next_edge_angle(&next_edge_heading), -20);
+
+        let edge_heading = EdgeHeading::with_start_and_end(350, 350);
+        let next_edge_heading = EdgeHeading::with_start_and_end(10, 10);
+        assert_eq!(edge_heading.next_edge_angle(&next_edge_heading), 20);
     }
 }
