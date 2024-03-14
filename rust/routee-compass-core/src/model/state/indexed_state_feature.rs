277c746eef6aab015ee969276807d2a0ce6e6463
rfitzger <robert.fitzgerald@nrel.gov>
2024-03-14 12:35:47 -0600

    rename indexed state feature

--- rust/routee-compass-core/src/model/state/indexed_state_feature.rs
+++ rust/routee-compass-core/src/model/state/indexed_state_feature.rs
@@ -6,15 +6,21 @@ use serde::{Deserialize, Serialize};
 /// simple record type which couples the state variable index with the feature
 /// representation for a given state variable in the StateModel.
 #[derive(Serialize, Deserialize, Clone, Debug)]
-pub struct StateModelEntry {
+pub struct IndexedStateFeature {
     pub index: usize,
     pub feature: StateFeature,
 }
 
-impl Display for StateModelEntry {
+impl Display for IndexedStateFeature {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let feature_str = serde_json::to_string(&self.feature).unwrap_or(String::from("<err!>"));
         let string = format!("{}: {}", self.index, feature_str);
         f.write_str(&string)
+    }
+}
+
+impl IndexedStateFeature {
+    pub fn new(index: usize, feature: StateFeature) -> IndexedStateFeature {
+        IndexedStateFeature { index, feature }
     }
 }
