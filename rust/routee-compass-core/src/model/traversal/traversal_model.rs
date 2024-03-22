f049e14030b75bddd8316f4536f42c7acda4d902
rfitzger <robert.fitzgerald@nrel.gov>
2024-03-22 12:50:12 -0600

    cleanup comments

--- rust/routee-compass-core/src/model/traversal/traversal_model.rs
+++ rust/routee-compass-core/src/model/traversal/traversal_model.rs
@@ -38,33 +38,6 @@ pub trait TraversalModel: Send + Sync {
         state_model: &StateModel,
     ) -> Result<(), TraversalModelError>;
 
-    // /// Updates the traversal state by accessing some destination edge
-    // /// when coming from some previous edge.
-    // ///
-    // /// These arguments appear in the network as:
-    // /// `(v1) -[prev]-> (v2) -[next]-> (v3)`
-    // /// Where `next` is the edge we want to access.
-    // ///
-    // /// # Arguments
-    // ///
-    // /// * `v1` - src of previous edge
-    // /// * `src` - previous edge
-    // /// * `v2` - src vertex of the next edge
-    // /// * `dst` - edge we are determining the cost to access
-    // /// * `v3` - dst vertex of the next edge
-    // /// * `state` - state of the search at the beginning of the dst edge
-    // ///
-    // /// # Returns
-    // ///
-    // /// Either an optional access result or an error. if there are no
-    // /// state updates due to access, None is returned.
-    // fn access_edge(
-    //     &self,
-    //     trajectory: (&Vertex, &Edge, &Vertex, &Edge, &Vertex),
-    //     state: &mut Vec<StateVar>,
-    //     state_model: &StateModel,
-    // ) -> Result<(), TraversalModelError>;
-
     /// Estimates the traversal state by traversing between two vertices without
     /// performing any graph traversals.
     ///
