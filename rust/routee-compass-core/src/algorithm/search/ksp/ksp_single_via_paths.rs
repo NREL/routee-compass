56b18c2e59a4ebc72d4d4acf14c6dc8ef4278a29
rfitzger <robert.fitzgerald@nrel.gov>
2024-04-23 16:35:16 -0600

    ksp single via paths algorithm

--- rust/routee-compass-core/src/algorithm/search/ksp/ksp_single_via_paths.rs
+++ rust/routee-compass-core/src/algorithm/search/ksp/ksp_single_via_paths.rs
@@ -29,7 +29,7 @@ pub fn run(
         trees: rev_trees,
         routes: _,
         iterations: rev_iterations,
-    } = underlying.run_vertex_oriented(source, Some(target), &Direction::Reverse, si)?;
+    } = underlying.run_vertex_oriented(target, Some(source), &Direction::Reverse, si)?;
     if fwd_trees.len() != 1 {
         Err(SearchError::InternalSearchError(format!(
             "ksp solver fwd trees count should be exactly 1, found {}",
@@ -50,23 +50,31 @@ pub fn run(
     })?;
 
     // find intersection vertices
-    let fwd_vertices = fwd_trees.iter().flatten().collect::<HashMap<_, _>>();
+    let rev_vertices = rev_trees.iter().flatten().collect::<HashMap<_, _>>();
     let mut intersection_queue: InternalPriorityQueue<VertexId, ReverseCost> =
         InternalPriorityQueue::default();
 
-    for (vertex_id, rev_branch) in rev_tree {
-        if let Some(fwd_branch) = fwd_vertices.get(&vertex_id) {
-            let total_cost =
-                fwd_branch.edge_traversal.total_cost() + rev_branch.edge_traversal.total_cost();
-            intersection_queue.push(*vertex_id, total_cost.into());
+    // valid intersection vertices should appear both as terminal vertices and lookup vertices in both trees
+    // - being a "terminal vertex" places them at the shared meeting location, terminus of each tree somewhere
+    // - being a "lookup vertex" means we can use them for backtracking forward and reverse paths
+    for (vertex_id, fwd_branch) in fwd_tree {
+        if let Some(rev_branch) = rev_vertices.get(&fwd_branch.terminal_vertex) {
+            if rev_vertices.contains_key(&vertex_id) {
+                let total_cost =
+                    fwd_branch.edge_traversal.total_cost() + rev_branch.edge_traversal.total_cost();
+                intersection_queue.push(*vertex_id, total_cost.into());
+            }
         }
     }
 
-    let mut solution: Vec<Vec<EdgeTraversal>> = vec![];
+    let tsp = backtrack::vertex_oriented_route(source, target, fwd_tree)?;
+    let mut solution: Vec<Vec<EdgeTraversal>> = vec![tsp];
+    let mut ksp_iterations: u64 = 0;
     loop {
         if solution.len() == k {
             break;
         }
+        ksp_iterations += 1;
         match intersection_queue.pop() {
             None => break,
             Some((intersection_vertex_id, _)) => {
@@ -87,11 +95,12 @@ pub fn run(
             }
         }
     }
+
     // combine all data into this result
     let result = SearchAlgorithmResult {
         trees: vec![fwd_tree.clone(), rev_tree.clone()], // todo: figure out how to avoid this clone
         routes: solution,
-        iterations: fwd_iterations + rev_iterations,
+        iterations: fwd_iterations + rev_iterations + ksp_iterations, // todo: figure out how to report individually
     };
     Ok(result)
 }
