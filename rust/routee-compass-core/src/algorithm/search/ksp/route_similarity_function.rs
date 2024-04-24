cccd3935515f3291dc20c82e3af839e2f8eedb7d
rfitzger <robert.fitzgerald@nrel.gov>
2024-04-24 15:44:44 -0600

    fix cos similarity implementation

--- rust/routee-compass-core/src/algorithm/search/ksp/route_similarity_function.rs
+++ rust/routee-compass-core/src/algorithm/search/ksp/route_similarity_function.rs
@@ -4,7 +4,7 @@ use crate::{
     algorithm::search::{
         edge_traversal::EdgeTraversal, search_error::SearchError, search_instance::SearchInstance,
     },
-    model::unit::as_f64::AsF64,
+    model::{road_network::edge_id::EdgeId, unit::as_f64::AsF64},
 };
 use serde::{Deserialize, Serialize};
 
@@ -64,41 +64,61 @@ impl RouteSimilarityFunction {
     ) -> Result<f64, SearchError> {
         match self {
             RouteSimilarityFunction::EdgeIdCosineSimilarity { threshold: _ } => {
-                let numer = a
-                    .iter()
-                    .map(|e| e.edge_id)
-                    .collect::<HashSet<_>>()
-                    .intersection(&b.iter().map(|e| e.edge_id).collect::<HashSet<_>>())
-                    .collect::<Vec<_>>()
-                    .len();
-                let denom = a.len() + b.len();
-                if denom == 0 {
-                    Ok(0.0)
-                } else {
-                    Ok(numer as f64 / denom as f64)
-                }
+                let unit_dist_fn = Box::new(|_| Ok(1.0));
+                cos_similarity(a, b, unit_dist_fn)
             }
             RouteSimilarityFunction::DistanceWeightedCosineSimilarity { threshold: _ } => {
-                let a_set = a
-                    .iter()
-                    .map(|e| {
-                        si.directed_graph
-                            .get_edge(e.edge_id)
-                            .map(|edge| (e.edge_id, edge.distance.as_f64()))
-                    })
-                    .collect::<Result<HashMap<_, _>, _>>()?;
-                let b_set = b
-                    .iter()
-                    .flat_map(|e| a_set.get(&e.edge_id).copied())
-                    .collect::<Vec<_>>();
-                let numer: f64 = b_set.into_iter().sum();
-                let denom = a.len() + b.len();
-                if denom == 0 {
-                    Ok(0.0)
-                } else {
-                    Ok(numer / denom as f64)
-                }
+                let dist_fn = Box::new(|edge_id| {
+                    si.directed_graph
+                        .get_edge(edge_id)
+                        .map(|edge| edge.distance.as_f64())
+                        .map_err(SearchError::GraphError)
+                });
+                cos_similarity(a, b, dist_fn)
             }
         }
     }
+}
+
+/// computes the cosine similarity of two routes using the provided distance function
+///
+/// # Arguments
+///
+/// * `a`       - this route
+/// * `b`       - that route
+/// * `dist_fn` - mapping from EdgeIds to some distance value
+///
+/// # Returns
+///
+/// the cosine similarity of the routes, a value from -1 to 1
+fn cos_similarity<'a>(
+    a: &[EdgeTraversal],
+    b: &[EdgeTraversal],
+    dist_fn: Box<dyn Fn(EdgeId) -> Result<f64, SearchError> + 'a>,
+) -> Result<f64, SearchError> {
+    let a_map = a
+        .iter()
+        .map(|e| dist_fn(e.edge_id).map(|dist| (e.edge_id, dist)))
+        .collect::<Result<HashMap<_, _>, _>>()?;
+    let b_map = b
+        .iter()
+        .map(|e| dist_fn(e.edge_id).map(|dist| (e.edge_id, dist)))
+        .collect::<Result<HashMap<_, _>, _>>()?;
+
+    let numer: f64 = a_map
+        .keys()
+        .collect::<HashSet<_>>()
+        .union(&b_map.keys().collect::<HashSet<_>>())
+        .map(|edge_id| {
+            let a_dist = a_map.get(edge_id).cloned().unwrap_or_default();
+            let b_dist = b_map.get(edge_id).cloned().unwrap_or_default();
+            a_dist * b_dist
+        })
+        .sum();
+
+    let denom_a: f64 = a_map.values().map(|d| d * d).sum();
+    let denom_b: f64 = b_map.values().map(|d| d * d).sum();
+    let denom = denom_a.sqrt() * denom_b.sqrt();
+    let cos_sim = numer / denom;
+    Ok(cos_sim)
 }
