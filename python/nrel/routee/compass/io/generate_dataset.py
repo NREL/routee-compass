037818bece0b53e548e854fc4ededca9de40377d
Nicholas Reinicke <Nicholas.Reinicke@nrel.gov>
2024-05-20 10:18:01 -0400

    formatting

--- python/nrel/routee/compass/io/generate_dataset.py
+++ python/nrel/routee/compass/io/generate_dataset.py
@@ -189,9 +189,9 @@ def generate_compass_dataset(
                 init_toml = toml.loads(f.read())
                 if filename == "osm_default_energy.toml":
                     if add_grade:
-                        init_toml["traversal"]["grade_table_input_input_file"] = (
-                            "edges-grade-enumerated.txt.gz"
-                        )
+                        init_toml["traversal"][
+                            "grade_table_input_input_file"
+                        ] = "edges-grade-enumerated.txt.gz"
                         init_toml["traversal"]["grade_table_grade_unit"] = "decimal"
             with open(output_directory / filename, "w") as f:
                 f.write(toml.dumps(init_toml))
