b5ea2b928578aeb324e355542e051b059e94cd65
Kyle Carow <kylecarow@gmail.com>
2024-05-21 11:42:00 -0400

    expose interp mod

--- rust/routee-compass-powertrain/src/routee/prediction/interpolation/interp.rs
+++ rust/routee-compass-powertrain/src/routee/prediction/interpolation/interp.rs
@@ -1,5 +1,5 @@
 use itertools::Itertools;
-use ndarray::prelude::*;
+use ndarray::{prelude::*, Slice};
 
 #[derive(Debug)]
 pub enum Interpolator {
--- rust/routee-compass-powertrain/src/routee/prediction/interpolation/mod.rs
+++ rust/routee-compass-powertrain/src/routee/prediction/interpolation/mod.rs
@@ -1,4 +1,4 @@
 pub mod bilinear_interp;
-// pub mod interp;
+pub mod interp;
 pub mod interpolation_speed_grade_model;
 pub mod utils;
