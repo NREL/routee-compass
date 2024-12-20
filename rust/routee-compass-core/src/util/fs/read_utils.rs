84c824d405ca2a17d99e769d34a49cb533d7368b
rfitzger <robert.fitzgerald@nrel.gov>
2024-12-20 14:26:34 -0700

    finalize bar on stderr with newline

--- rust/routee-compass-core/src/util/fs/read_utils.rs
+++ rust/routee-compass-core/src/util/fs/read_utils.rs
@@ -16,10 +16,89 @@ type RawCallback<'a> = Option<Box<dyn FnMut() + 'a>>;
 /// if COMPASS_PROGRESS=false, the bar is deactivated, otherwise it runs.
 const COMPASS_PROGRESS: &str = "COMPASS_PROGRESS";
 
+/// reads a csv file into a vector. not space-optimized since size is not
+/// known.
+pub fn from_csv<'a, T>(
+    filepath: &dyn AsRef<Path>,
+    has_headers: bool,
+    progress: Option<BarBuilder>,
+    callback: CsvCallback<'a, T>,
+) -> Result<Box<[T]>, csv::Error>
+where
+    T: serde::de::DeserializeOwned + 'a,
+{
+    let bar_opt = build_bar(progress);
+    let finalize_bar = bar_opt.is_some();
+
+    let row_callback: CsvCallback<'a, T> = match (callback, bar_opt) {
+        (None, None) => None,
+        (None, Some(mut bar)) => Some(Box::new(move |_| {
+            let _ = bar.update(1);
+        })),
+        (Some(cb), None) => Some(cb),
+        (Some(mut cb), Some(mut bar)) => Some(Box::new(move |t| {
+            cb(t);
+            let _ = bar.update(1);
+        })),
+    };
+
+    let iter: Box<dyn Iterator<Item = Result<T, csv::Error>>> =
+        iterator_from_csv(filepath, has_headers, row_callback)?;
+    let result = iter
+        .into_iter()
+        .collect::<Result<Vec<T>, csv::Error>>()?
+        .into_boxed_slice();
+
+    if finalize_bar {
+        eprintln!();
+    }
+
+    Ok(result)
+}
+
+/// reads in a raw file and deserializes each line of the file into a type T
+/// using the provided operation.
+/// inspects the file to determine if it should read as a raw or gzip stream.
+/// the row index (starting from zero) is passed to the deserialization op
+/// as in most cases, the row number is an id.
+pub fn read_raw_file<F, T>(
+    filepath: F,
+    op: impl Fn(usize, String) -> Result<T, io::Error>,
+    progress: Option<BarBuilder>,
+    callback: Option<Box<dyn FnMut()>>,
+) -> Result<Box<[T]>, io::Error>
+where
+    F: AsRef<Path>,
+{
+    let bar_opt = build_bar(progress);
+    let finalize_bar = bar_opt.is_some();
+
+    let row_callback: RawCallback = match (callback, bar_opt) {
+        (None, None) => None,
+        (None, Some(mut bar)) => Some(Box::new(move || {
+            let _ = bar.update(1);
+        })),
+        (Some(cb), None) => Some(cb),
+        (Some(mut cb), Some(mut bar)) => Some(Box::new(move || {
+            cb();
+            let _ = bar.update(1);
+        })),
+    };
+
+    let result = if fs_utils::is_gzip(filepath.as_ref()) {
+        Ok(read_gzip(filepath, op, row_callback)?)
+    } else {
+        Ok(read_regular(filepath, op, row_callback)?)
+    };
+    if finalize_bar {
+        eprintln!();
+    }
+    result
+}
+
 /// reads from a CSV into an iterator of T records.
 /// building the iterator may fail with an io::Error.
 /// each row hasn't yet been decoded so it is provided in a Result<T, csv::Error>
-///
 pub fn iterator_from_csv<'a, F, T>(
     filepath: F,
     has_headers: bool,
@@ -49,75 +128,6 @@ where
         });
 
     Ok(Box::new(reader))
-}
-
-/// reads a csv file into a vector. not space-optimized since size is not
-/// known.
-pub fn from_csv<'a, T>(
-    filepath: &dyn AsRef<Path>,
-    has_headers: bool,
-    progress: Option<BarBuilder>,
-    callback: CsvCallback<'a, T>,
-) -> Result<Box<[T]>, csv::Error>
-where
-    T: serde::de::DeserializeOwned + 'a,
-{
-    let bar_opt = build_bar(progress);
-
-    let row_callback: CsvCallback<'a, T> = match (callback, bar_opt) {
-        (None, None) => None,
-        (None, Some(mut bar)) => Some(Box::new(move |_| {
-            let _ = bar.update(1);
-        })),
-        (Some(cb), None) => Some(cb),
-        (Some(mut cb), Some(mut bar)) => Some(Box::new(move |t| {
-            cb(t);
-            let _ = bar.update(1);
-        })),
-    };
-
-    let iter: Box<dyn Iterator<Item = Result<T, csv::Error>>> =
-        iterator_from_csv(filepath, has_headers, row_callback)?;
-    let result = iter
-        .into_iter()
-        .collect::<Result<Vec<T>, csv::Error>>()?
-        .into_boxed_slice();
-    Ok(result)
-}
-
-/// reads in a raw file and deserializes each line of the file into a type T
-/// using the provided operation.
-/// inspects the file to determine if it should read as a raw or gzip stream.
-/// the row index (starting from zero) is passed to the deserialization op
-/// as in most cases, the row number is an id.
-pub fn read_raw_file<F, T>(
-    filepath: F,
-    op: impl Fn(usize, String) -> Result<T, io::Error>,
-    progress: Option<BarBuilder>,
-    callback: Option<Box<dyn FnMut()>>,
-) -> Result<Box<[T]>, io::Error>
-where
-    F: AsRef<Path>,
-{
-    let bar_opt = build_bar(progress);
-
-    let row_callback: RawCallback = match (callback, bar_opt) {
-        (None, None) => None,
-        (None, Some(mut bar)) => Some(Box::new(move || {
-            let _ = bar.update(1);
-        })),
-        (Some(cb), None) => Some(cb),
-        (Some(mut cb), Some(mut bar)) => Some(Box::new(move || {
-            cb();
-            let _ = bar.update(1);
-        })),
-    };
-
-    if fs_utils::is_gzip(filepath.as_ref()) {
-        Ok(read_gzip(filepath, op, row_callback)?)
-    } else {
-        Ok(read_regular(filepath, op, row_callback)?)
-    }
 }
 
 fn read_regular<'a, F, T>(
