5d6d5566d9f4beafa528c407945005e903ba0e79
rfitzger <robert.fitzgerald@nrel.gov>
2024-12-31 18:43:19 -0700

    remove GraphBuilder (artifact of Graph Trait)

--- rust/routee-compass/src/app/compass/config/mod.rs
+++ rust/routee-compass/src/app/compass/config/mod.rs
@@ -6,6 +6,5 @@ pub mod compass_configuration_field;
 pub mod config_json_extension;
 pub mod cost_model;
 pub mod frontier_model;
-pub mod graph_builder;
 pub mod termination_model_builder;
 pub mod traversal_model;
