a44fa3b231d966fcc9a6fc314a3a27aa29a22cd6
rfitzger <robert.fitzgerald@nrel.gov>
2025-02-21 16:25:13 -0700

    fix flipped energy rate unit deserialization

--- rust/routee-compass-core/src/model/unit/energy_rate_unit.rs
+++ rust/routee-compass-core/src/model/unit/energy_rate_unit.rs
@@ -4,23 +4,23 @@ use serde::{Deserialize, Deserializer, Serialize};
 use std::str::FromStr;
 
 #[derive(Debug, Clone, Eq, PartialEq, Copy)]
-pub struct EnergyRateUnit(pub EnergyUnit, pub DistanceUnit);
+pub struct EnergyRateUnit(pub DistanceUnit, pub EnergyUnit);
 
 impl EnergyRateUnit {
     /// energy rates are defined with respect to a distance unit
     pub fn associated_distance_unit(&self) -> DistanceUnit {
-        self.1
+        self.0
     }
 
     pub fn associated_energy_unit(&self) -> EnergyUnit {
-        self.0
+        self.1
     }
 }
 
 impl std::fmt::Display for EnergyRateUnit {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let (du, tu) = (self.0, self.1);
-        write!(f, "{}/{}", du, tu)
+        let (du, eu) = (self.0, self.1);
+        write!(f, "{}/{}", du, eu)
     }
 }
 
@@ -29,23 +29,23 @@ impl FromStr for EnergyRateUnit {
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         match s.split("/").collect_vec()[..] {
-            [eu_str, du_str] => {
-                let eu = EnergyUnit::from_str(eu_str).map_err(|e| {
-                    format!(
-                        "energy rate unit has invalid energy unit value '{}', error: {}",
-                        eu_str, e
-                    )
-                })?;
+            [du_str, eu_str] => {
                 let du = DistanceUnit::from_str(eu_str).map_err(|e| {
                     format!(
                         "energy rate unit has invalid distance unit value '{}', error: {}",
                         du_str, e
                     )
                 })?;
-                Ok(EnergyRateUnit(eu, du))
+                let eu = EnergyUnit::from_str(eu_str).map_err(|e| {
+                    format!(
+                        "energy rate unit has invalid energy unit value '{}', error: {}",
+                        eu_str, e
+                    )
+                })?;
+                Ok(EnergyRateUnit(du, eu))
             }
             _ => Err(format!(
-                "expected energy rate unit in the format '<distance>/<time>', found: {}",
+                "expected energy rate unit in the format '<distance>/<energy>', found: {}",
                 s
             )),
         }
