c3404165e9046619dff67a060ca76f982b8fd0d2
rfitzger <robert.fitzgerald@nrel.gov>
2025-04-22 16:27:49 -0600

    revised distance traversal modeling

--- rust/routee-compass-core/src/model/traversal/default/distance_traversal_model.rs
+++ rust/routee-compass-core/src/model/traversal/default/distance_traversal_model.rs
@@ -6,11 +6,12 @@ use crate::model::state::StateModel;
 use crate::model::state::StateVariable;
 use crate::model::traversal::traversal_model::TraversalModel;
 use crate::model::traversal::traversal_model_error::TraversalModelError;
-use crate::model::unit::{baseunit, Convert, DistanceUnit};
+use crate::model::unit::{baseunit, Convert, Distance, DistanceUnit};
 use crate::util::geo::haversine;
 
-/// A simple traversal model that uses the edge distance as the cost of traversal.
+/// a model for traversing edges based on distance.
 pub struct DistanceTraversalModel {
+    /// this is the unit used to store distance in the state vector
     distance_unit: DistanceUnit,
 }
 
@@ -18,11 +19,15 @@ impl DistanceTraversalModel {
     pub fn new(distance_unit: DistanceUnit) -> DistanceTraversalModel {
         DistanceTraversalModel { distance_unit }
     }
-    const DISTANCE: &'static str = "distance";
+    const TRIP_DISTANCE: &'static str = "trip_distance";
+    const LEG_DISTANCE: &'static str = "leg_distance";
 }
 
 impl TraversalModel for DistanceTraversalModel {
-    //
+    /// traverses a graph edge and updates the state vector with the distance.
+    /// the distance values are directly available on the [`Graph`] model edges.
+    ///
+    /// [Graph]: crate::model::network::Graph
     fn traverse_edge(
         &self,
         trajectory: (&Vertex, &Edge, &Vertex),
@@ -32,15 +37,11 @@ impl TraversalModel for DistanceTraversalModel {
         let (_, edge, _) = trajectory;
         let mut distance = Cow::Borrowed(&edge.distance);
         baseunit::DISTANCE_UNIT.convert(&mut distance, &self.distance_unit)?;
-        state_model.add_distance(
-            state,
-            &Self::DISTANCE.into(),
-            &distance,
-            &self.distance_unit,
-        )?;
+        state_model.add_distance(state, Self::TRIP_DISTANCE, &distance, &self.distance_unit)?;
         Ok(())
     }
 
+    /// uses a haversine distance to estimate the distance between two vertices.
     fn estimate_traversal(
         &self,
         od: (&Vertex, &Vertex),
@@ -56,17 +57,31 @@ impl TraversalModel for DistanceTraversalModel {
                         src, dst, e
                     ))
                 })?;
-        state_model.add_distance(
-            state,
-            &Self::DISTANCE.into(),
-            &distance,
-            &self.distance_unit,
-        )?;
+        state_model.add_distance(state, Self::TRIP_DISTANCE, &distance, &self.distance_unit)?;
+        state_model.set_distance(state, Self::LEG_DISTANCE, &distance, &self.distance_unit)?;
         Ok(())
     }
 
-    /// no additional state features are needed
-    fn state_features(&self) -> Vec<(String, StateFeature)> {
+    fn input_features(&self) -> Vec<(String, StateFeature)> {
         vec![]
+    }
+
+    fn output_features(&self) -> Vec<(String, StateFeature)> {
+        vec![
+            (
+                String::from(Self::TRIP_DISTANCE),
+                StateFeature::Distance {
+                    distance_unit: self.distance_unit,
+                    initial: Distance::ZERO,
+                },
+            ),
+            (
+                String::from(Self::LEG_DISTANCE),
+                StateFeature::Distance {
+                    distance_unit: self.distance_unit,
+                    initial: Distance::ZERO,
+                },
+            ),
+        ]
     }
 }
