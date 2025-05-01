525d45a9f9fa6635c5b63814350577ca2c3cf444
rfitzger <robert.fitzgerald@nrel.gov>
2025-05-01 11:06:48 -0600

    update soc without state context

--- rust/routee-compass-powertrain/src/model/energy_model_ops.rs
+++ rust/routee-compass-powertrain/src/model/energy_model_ops.rs
@@ -1,11 +1,61 @@
+use std::borrow::Cow;
+
 use routee_compass_core::model::{
-    access::default::turn_delays::EdgeHeading, network::edge_id::EdgeId,
-    traversal::TraversalModelError, unit::Grade,
+    access::default::turn_delays::EdgeHeading,
+    network::edge_id::EdgeId,
+    traversal::TraversalModelError,
+    unit::{AsF64, Convert, Energy, EnergyUnit, Grade, UnitError},
 };
 
 /// used as a replacement for zero in energy calculations
 /// where zero is not a valid value.
 pub const SAFE_MIN_ENERGY: f64 = 1e-9;
+
+/// updates the SOC feature for a vehicle type with a battery based on the
+/// state, the energy delta, and max battery capacity.
+///
+/// note: if delta is negative, this is a regenerative braking event.
+///
+/// # Arguments
+///
+/// * `delta`        - change in energy
+/// * `maximum_energy` - maximum energy for this vehicle
+pub fn update_soc_percent(
+    start_soc: &f64,
+    energy_consumption: (&Energy, &EnergyUnit),
+    maximum_energy: (&Energy, &EnergyUnit),
+) -> Result<f64, UnitError> {
+    let (delta_energy, delta_unit) = energy_consumption;
+    let (max_energy, max_unit) = maximum_energy;
+    let mut delta = Cow::Borrowed(delta_energy);
+    delta_unit.convert(&mut delta, max_unit)?;
+    let start_battery = Energy::from(max_energy.as_f64() * (start_soc / 100.0));
+    let current_energy = start_battery - *delta_energy;
+    let percent_remaining = (current_energy.as_f64() / max_energy.as_f64()) * 100.0;
+    Ok(percent_remaining.clamp(0.0, 100.0))
+}
+
+pub fn soc_from_energy(
+    energy: (&Energy, &EnergyUnit),
+    maximum_energy: (&Energy, &EnergyUnit),
+) -> Result<f64, String> {
+    let (e, eu) = energy;
+    let (me, meu) = maximum_energy;
+    let mut e_mut = Cow::Borrowed(e);
+    eu.convert(&mut e_mut, meu).map_err(|e| format!("while converting energy to soc, failed to match energy units of max value and current value: {}", e))?;
+    let energy = e_mut.into_owned();
+    if energy < Energy::ZERO {
+        return Ok(0.0);
+    }
+    if me < &energy {
+        return Err(format!(
+            "vehicle energy {} is greater than battery capacity {}",
+            energy, me
+        ));
+    }
+    let soc = energy.as_f64() / me.as_f64();
+    Ok(soc)
+}
 
 /// look up the grade from the grade table
 pub fn get_grade(
