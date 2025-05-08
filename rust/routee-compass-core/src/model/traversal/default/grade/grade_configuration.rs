a3248a458c3b430f4c742d547151f56c49aad9e2
rfitzger <robert.fitzgerald@nrel.gov>
2025-05-08 10:43:09 -0600

    allow building dummy grade models (stopgap)

--- rust/routee-compass-core/src/model/traversal/default/grade/grade_configuration.rs
+++ rust/routee-compass-core/src/model/traversal/default/grade/grade_configuration.rs
@@ -1,14 +1,14 @@
-use crate::model::unit::{DistanceUnit, GradeUnit};
+use crate::model::unit::GradeUnit;
 use serde::{Deserialize, Serialize};
 
 /// provides configuration for instantiating the grade engine used in grade modeling.
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct GradeConfiguration {
     /// file with dense mapping from edge id to grade value
-    pub grade_input_file: String,
+    /// note: this is optional as a stop-gap to allow for loading energy models that do not require grade.
+    /// this is because we currently cannot define input features that are _optional_, and so
+    /// if a dummy grade model were not provided, energy models would fail to build.
+    pub grade_input_file: Option<String>,
     /// type of grade values in file
     pub grade_unit: GradeUnit,
-    /// distance unit used when recording elevation values. if not provided,
-    /// application will use "feet".
-    pub elevation_unit: Option<DistanceUnit>,
 }
--- rust/routee-compass-core/src/model/traversal/default/grade/grade_traversal_engine.rs
+++ rust/routee-compass-core/src/model/traversal/default/grade/grade_traversal_engine.rs
@@ -11,44 +11,54 @@ use kdam::Bar;
 use std::sync::Arc;
 
 pub struct GradeTraversalEngine {
-    pub grade_by_edge_id: Arc<Box<[Grade]>>,
+    pub grade_by_edge_id: Option<Arc<Box<[Grade]>>>,
     pub grade_unit: GradeUnit,
 }
 
 impl GradeTraversalEngine {
     pub fn new(config: &GradeConfiguration) -> Result<GradeTraversalEngine, TraversalModelError> {
-        let grade_table: Box<[Grade]> = read_utils::read_raw_file(
-            &config.grade_input_file,
-            read_decoders::default,
-            Some(Bar::builder().desc("link grades")),
-            None,
-        )
-        .map_err(|e| {
-            TraversalModelError::BuildError(format!(
-                "failure reading grade table {} due to {}",
-                config.grade_input_file.clone(),
-                e
-            ))
-        })?;
+        match &config.grade_input_file {
+            None => Ok(Self {
+                grade_by_edge_id: None,
+                grade_unit: config.grade_unit,
+            }),
+            Some(grade_input_file) => {
+                let grade_table: Box<[Grade]> = read_utils::read_raw_file(
+                    grade_input_file,
+                    read_decoders::default,
+                    Some(Bar::builder().desc("link grades")),
+                    None,
+                )
+                .map_err(|e| {
+                    TraversalModelError::BuildError(format!(
+                        "failure reading grade table {} due to {}",
+                        grade_input_file.clone(),
+                        e
+                    ))
+                })?;
 
-        let engine = GradeTraversalEngine {
-            grade_by_edge_id: Arc::new(grade_table),
-            grade_unit: config.grade_unit,
-        };
+                let engine = GradeTraversalEngine {
+                    grade_by_edge_id: Some(Arc::new(grade_table)),
+                    grade_unit: config.grade_unit,
+                };
 
-        Ok(engine)
+                Ok(engine)
+            }
+        }
     }
 
     pub fn get_grade(&self, edge_id: EdgeId) -> Result<Grade, TraversalModelError> {
-        let grade: &Grade = self
-            .grade_by_edge_id
-            .get(edge_id.as_usize())
-            .ok_or_else(|| {
-                TraversalModelError::TraversalModelFailure(format!(
-                    "missing index {} from grade table",
-                    edge_id
-                ))
-            })?;
-        Ok(*grade)
+        match &self.grade_by_edge_id {
+            None => return Ok(Grade::ZERO),
+            Some(table) => {
+                let grade: &Grade = table.get(edge_id.as_usize()).ok_or_else(|| {
+                    TraversalModelError::TraversalModelFailure(format!(
+                        "missing index {} from grade table",
+                        edge_id
+                    ))
+                })?;
+                Ok(*grade)
+            }
+        }
     }
 }
