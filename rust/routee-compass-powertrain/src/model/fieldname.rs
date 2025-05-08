df986d7c2daf8bc45267359d51359e3cae54fee9
rfitzger <robert.fitzgerald@nrel.gov>
2025-05-08 13:25:32 -0600

    clippy

--- rust/routee-compass-powertrain/src/model/fieldname.rs
+++ rust/routee-compass-powertrain/src/model/fieldname.rs
@@ -7,9 +7,9 @@
 //!  - `access_*` - state values for accessing a graph edge
 //!  - `trip_*` - state values for a trip
 
-pub const EDGE_ENERGY_LIQUID: &'static str = "edge_energy_liquid";
-pub const TRIP_ENERGY_LIQUID: &'static str = "trip_energy_liquid";
-pub const EDGE_ENERGY_ELECTRIC: &'static str = "edge_energy_electric";
-pub const TRIP_ENERGY_ELECTRIC: &'static str = "trip_energy_electric";
-pub const TRIP_SOC: &'static str = "trip_soc";
+pub const EDGE_ENERGY_LIQUID: &str = "edge_energy_liquid";
+pub const TRIP_ENERGY_LIQUID: &str = "trip_energy_liquid";
+pub const EDGE_ENERGY_ELECTRIC: &str = "edge_energy_electric";
+pub const TRIP_ENERGY_ELECTRIC: &str = "trip_energy_electric";
+pub const TRIP_SOC: &str = "trip_soc";
 pub use routee_compass_core::model::traversal::default::fieldname::*;
