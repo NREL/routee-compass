0faa62c451cc14238c44a413e58c96df5f503e2b
yamilbknsu <yaessus@ncsu.edu>
2025-05-19 15:41:04 -0400

    Added `Joules` to `EnergyUnit`

--- rust/routee-compass-core/src/model/unit/energy_unit.rs
+++ rust/routee-compass-core/src/model/unit/energy_unit.rs
@@ -18,7 +18,7 @@ pub enum EnergyUnit {
     Gasoline(VolumeUnit),
     /// 1 [VolumeUnit] Diesel fuel
     Diesel(VolumeUnit),
-    // Joules
+    Joules
     // BTU
     // eV
     // calorie (small c)
@@ -29,42 +29,90 @@ impl Convert<Energy> for EnergyUnit {
         use EnergyUnit as S;
         use VolumeUnit as V;
         let conversion_factor = match (self, to) {
-            (S::GallonsGasoline, S::GallonsGasoline) => None,
-            (S::GallonsGasoline, S::KilowattHours) => Some(32.26),
-            (S::GallonsGasoline, S::LitersGasoline) => Some(3.78541),
-            (S::GallonsGasoline, S::LitersDiesel) => Some(0.866 * 3.78541),
-            (S::KilowattHours, S::GallonsGasoline) => Some(0.031),
+            (S::Gasoline(V::GallonsUs), S::Gasoline(V::GallonsUs)) => None,             // (S::GallonsGasoline, S::GallonsGasoline) => None,
+            (S::Gasoline(V::GallonsUs), S::Gasoline(V::GallonsUk)) => Some(0.832674),
+            (S::Gasoline(V::GallonsUs), S::Gasoline(V::Liters)) => Some(3.78541),       // (S::GallonsGasoline, S::LitersGasoline) => Some(3.78541),
+            (S::Gasoline(V::GallonsUs), S::Diesel(V::GallonsUs)) => Some(0.866),        // (S::GallonsGasoline, S::GallonsDiesel) => Some(0.866),
+            // GG_us->GD_uk: GG_us->GG_uk->GD_uk
+            (S::Gasoline(V::GallonsUs), S::Diesel(V::GallonsUk)) => Some(0.832674 * 0.866),
+            // GG->LD: GG -> GD -> LD
+            (S::Gasoline(V::GallonsUs), S::Diesel(V::Liters)) => Some(0.866 * 3.78541), // (S::GallonsGasoline, S::LitersDiesel) => Some(0.866 * 3.78541),
+            (S::Gasoline(V::GallonsUs), S::KilowattHours) => Some(32.26),               // (S::GallonsGasoline, S::KilowattHours) => Some(32.26),
+            (S::Gasoline(V::GallonsUs), S::Joules) => Some(32.26 * 3_600_000.),
+            
+            (S::Gasoline(V::Liters), S::Gasoline(V::GallonsUs)) => Some(0.264172),      // (S::LitersGasoline, S::GallonsGasoline) => Some(0.264172),
+            (S::Gasoline(V::Liters), S::Gasoline(V::GallonsUk)) => Some(0.219969),
+            (S::Gasoline(V::Liters), S::Gasoline(V::Liters)) => None,                   // (S::LitersGasoline, S::LitersGasoline) => None,
+            // LG->GD: LG -> LD -> GD
+            (S::Gasoline(V::Liters), S::Diesel(V::GallonsUs)) => Some(0.264172 * 0.866),// (S::LitersGasoline, S::GallonsDiesel) => Some(0.264172 * 0.866),
+            // LG->GD_uk: LG->GD_us->GD_uk
+            (S::Gasoline(V::Liters), S::Diesel(V::GallonsUk)) => Some(0.264172 * 0.866 * 0.832674),
+            // LG->LD: LG -> GG -> GD -> LD
+            (S::Gasoline(V::Liters), S::Diesel(V::Liters)) => Some(0.866),              // (S::LitersGasoline, S::LitersDiesel) => Some(0.866),
+            // LG->KWH: LG -> GG -> KWH
+            (S::Gasoline(V::Liters), S::KilowattHours) => Some(0.264172 * 32.26),       // (S::LitersGasoline, S::KilowattHours) => Some(0.264172 * 32.26),
+            (S::Gasoline(V::Liters), S::Joules) => Some(0.264172 * 32.26 * 3_600_000.),       
+            
+            (S::Diesel(V::GallonsUs), S::Gasoline(V::GallonsUs)) => Some(1.155),        // (S::GallonsDiesel, S::GallonsGasoline) => Some(1.155),
+            (S::Diesel(V::GallonsUs), S::Gasoline(V::GallonsUk)) => Some(1.155 * 0.832674),
+            // GD->LG: GD -> GG -> LG
+            (S::Diesel(V::GallonsUs), S::Gasoline(V::Liters)) => Some(1.155 * 3.78541), // (S::GallonsDiesel, S::LitersGasoline) => Some(1.155 * 3.78541),
+            (S::Diesel(V::GallonsUs), S::Diesel(V::GallonsUs)) => None,                 // (S::GallonsDiesel, S::GallonsDiesel) => None,
+            (S::Diesel(V::GallonsUs), S::Diesel(V::GallonsUk)) => Some(0.832674),
+            (S::Diesel(V::GallonsUs), S::Diesel(V::Liters)) => Some(3.78541),           // (S::GallonsDiesel, S::LitersDiesel) => Some(3.78541),
+            (S::Diesel(V::GallonsUs), S::KilowattHours) => Some(40.7),                  // (S::GallonsDiesel, S::KilowattHours) => Some(40.7),
+            (S::Diesel(V::GallonsUs), S::Joules) => Some(40.7 * 3_600_000.),
+            
+            (S::Diesel(V::Liters), S::Gasoline(V::GallonsUs)) => Some(0.264172 * 1.155),// (S::LitersDiesel, S::GallonsGasoline) => Some(0.264172 * 1.155),
+            (S::Diesel(V::Liters), S::Gasoline(V::GallonsUk)) => Some(0.264172 * 1.155 * 0.832674),
+            // LD->LG: LD -> GD -> GG -> LG
+            (S::Diesel(V::Liters), S::Gasoline(V::Liters)) => Some(1.155),              // (S::LitersDiesel, S::LitersGasoline) => Some(1.155),
+            // LD->GG: LD -> LG -> GG
+            (S::Diesel(V::Liters), S::Diesel(V::GallonsUs)) => Some(0.264172),          // (S::LitersDiesel, S::GallonsDiesel) => Some(0.264172),
+            (S::Diesel(V::Liters), S::Diesel(V::GallonsUk)) => Some(0.264172 * 0.832674),
+            (S::Diesel(V::Liters), S::Diesel(V::Liters)) => None,                       // (S::LitersDiesel, S::LitersDiesel) => None,
+            // LD->KWH: LD -> GD -> KWH
+            (S::Diesel(V::Liters), S::KilowattHours) => Some(0.264172 * 40.7),          // (S::LitersDiesel, S::KilowattHours) => Some(0.264172 * 40.7),
+            (S::Diesel(V::Liters), S::Joules) => Some(0.264172 * 40.7 * 3_600_000.),
+            
+            (S::KilowattHours, S::Gasoline(V::GallonsUs)) => Some(0.031),               // (S::KilowattHours, S::GallonsGasoline) => Some(0.031),
+            (S::KilowattHours, S::Gasoline(V::GallonsUk)) => Some(0.031 * 0.832674),
+            // KWH->LG: KWH -> GG -> LG
+            (S::KilowattHours, S::Gasoline(V::Liters)) => Some(0.031 * 3.78541),        // (S::KilowattHours, S::LitersGasoline) => Some(0.031 * 3.78541),
+            (S::KilowattHours, S::Diesel(V::GallonsUs)) => Some(0.02457),               // (S::KilowattHours, S::GallonsDiesel) => Some(0.02457),
+            (S::KilowattHours, S::Diesel(V::GallonsUk)) => Some(0.02457 * 0.832674),
+            // KWH->LD: KWH -> GD -> LD
+            (S::KilowattHours, S::Diesel(V::Liters)) => Some(0.02457 * 3.78541),        // (S::KilowattHours, S::LitersDiesel) => Some(0.02457 * 3.78541),
             (S::KilowattHours, S::KilowattHours) => None,
-            (S::KilowattHours, S::LitersGasoline) => Some(0.031 * 3.78541),
-            (S::KilowattHours, S::LitersDiesel) => Some(0.02457 * 3.78541),
-            (S::GallonsDiesel, S::GallonsDiesel) => None,
-            (S::GallonsDiesel, S::KilowattHours) => Some(40.7),
-            (S::GallonsDiesel, S::LitersGasoline) => Some(1.155 * 3.78541),
-            (S::GallonsDiesel, S::LitersDiesel) => Some(3.78541),
-            (S::KilowattHours, S::GallonsDiesel) => Some(0.02457),
-            (S::GallonsDiesel, S::GallonsGasoline) => Some(1.155),
-            (S::GallonsGasoline, S::GallonsDiesel) => Some(0.866),
-            (S::LitersGasoline, S::LitersGasoline) => None,
-            (S::LitersGasoline, S::LitersDiesel) => Some(0.866),
-            (S::LitersGasoline, S::GallonsGasoline) => Some(0.264172),
-            (S::LitersGasoline, S::GallonsDiesel) => Some(0.264172 * 0.866),
-            (S::LitersGasoline, S::KilowattHours) => Some(0.264172 * 32.26),
-            (S::LitersDiesel, S::LitersDiesel) => None,
-            (S::LitersDiesel, S::LitersGasoline) => Some(1.155),
-            (S::LitersDiesel, S::GallonsGasoline) => Some(0.264172 * 1.155),
-            (S::LitersDiesel, S::GallonsDiesel) => Some(0.264172),
-            (S::LitersDiesel, S::KilowattHours) => Some(0.264172 * 40.7),
-            (S::GallonsGasoline, S::GallonsGasolineEquivalent) => None,
-            (S::GallonsDiesel, S::GallonsGasolineEquivalent) => Some(1.14),
-            (S::KilowattHours, S::GallonsGasolineEquivalent) => Some(0.03),
-            (S::LitersGasoline, S::GallonsGasolineEquivalent) => Some(0.264172),
-            (S::LitersDiesel, S::GallonsGasolineEquivalent) => Some(3.78541 * 1.14),
-            (S::GallonsGasolineEquivalent, S::GallonsGasoline) => None,
-            (S::GallonsGasolineEquivalent, S::GallonsDiesel) => Some(0.8771929825),
-            (S::GallonsGasolineEquivalent, S::KilowattHours) => Some(33.3333333333),
-            (S::GallonsGasolineEquivalent, S::LitersGasoline) => todo!(),
-            (S::GallonsGasolineEquivalent, S::LitersDiesel) => todo!(),
-            (S::GallonsGasolineEquivalent, S::GallonsGasolineEquivalent) => None,
+            (S::KilowattHours, S::Joules) => Some(3_600_000.),
+            
+            (S::Gasoline(V::GallonsUk), S::Gasoline(V::GallonsUs)) => Some(1.20095),
+            (S::Gasoline(V::GallonsUk), S::Gasoline(V::GallonsUk)) => None,
+            (S::Gasoline(V::GallonsUk), S::Gasoline(V::Liters)) => Some(4.54609),
+            (S::Gasoline(V::GallonsUk), S::Diesel(V::GallonsUs)) => Some(1.20095 * 0.866),
+            (S::Gasoline(V::GallonsUk), S::Diesel(V::GallonsUk)) => Some(0.866),
+            (S::Gasoline(V::GallonsUk), S::Diesel(V::Liters)) => Some(4.54609 * 0.866),
+            (S::Gasoline(V::GallonsUk), S::KilowattHours) => Some(1.20095 * 32.26),
+            (S::Gasoline(V::GallonsUk), S::Joules) => Some(1.20095 * 32.26 * 3_600_000.),
+            
+            (S::Diesel(V::GallonsUk), S::Gasoline(V::GallonsUs)) => Some(1.20095 * 1.155),
+            (S::Diesel(V::GallonsUk), S::Gasoline(V::GallonsUk)) => Some(1.155),
+            (S::Diesel(V::GallonsUk), S::Gasoline(V::Liters)) => Some(1.155 * 4.54609),
+            (S::Diesel(V::GallonsUk), S::Diesel(V::GallonsUs)) => Some(1.20095),
+            (S::Diesel(V::GallonsUk), S::Diesel(V::GallonsUk)) => None,
+            (S::Diesel(V::GallonsUk), S::Diesel(V::Liters)) => Some(4.54609),
+            (S::Diesel(V::GallonsUk), S::KilowattHours) => Some(1.20095 * 40.7),
+            (S::Diesel(V::GallonsUk), S::Joules) => Some(1.20095 * 40.7 * 3_600_000.),
+
+            // These are all transformed to kWh and then apply the kWh -> * conversion
+            (S::Joules, S::Gasoline(V::GallonsUs)) => Some(0.000000277778 * 0.031),
+            (S::Joules, S::Gasoline(V::GallonsUk)) => Some(0.000000277778 * 0.031 * 0.832674),
+            (S::Joules, S::Gasoline(V::Liters)) => Some(0.000000277778 * 0.031 * 3.78541),
+            (S::Joules, S::Diesel(V::GallonsUs)) => Some(0.000000277778 * 0.02457),
+            (S::Joules, S::Diesel(V::GallonsUk)) => Some(0.000000277778 * 0.02457 * 0.832674),
+            (S::Joules, S::Diesel(V::Liters)) => Some(0.000000277778 * 0.02457 * 3.78541),
+            (S::Joules, S::KilowattHours) => Some(0.000000277778),
+            (S::Joules, S::Joules) => None,
         };
         if let Some(factor) = conversion_factor {
             let updated = Energy::from(value.as_ref().as_f64() * factor);
