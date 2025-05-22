bd4bc6c0ad8fe11d2fc76178ad61d4a04bbe568f
rfitzger <robert.fitzgerald@nrel.gov>
2025-05-22 15:13:26 -0600

    additional default parsers

--- rust/routee-compass-core/src/util/fs/read_decoders.rs
+++ rust/routee-compass-core/src/util/fs/read_decoders.rs
@@ -1,3 +1,5 @@
+use crate::model::state::StateVariable;
+
 /// default read decoder for an arbitrary type if the read operation
 /// can be performed via the FromStr trait for type T
 pub fn default<T>(_idx: usize, row: String) -> Result<T, std::io::Error>
@@ -19,4 +21,51 @@ pub fn u8(_idx: usize, row: String) -> Result<u8, std::io::Error> {
         let msg = format!("failure decoding row {} due to: {:}", row, e);
         std::io::Error::new(std::io::ErrorKind::InvalidData, msg)
     })
+}
+
+pub fn f64(_idx: usize, row: String) -> Result<f64, std::io::Error> {
+    row.parse::<f64>().map_err(|e| {
+        let msg = format!("failure decoding row {} due to: {:}", row, e);
+        std::io::Error::new(std::io::ErrorKind::InvalidData, msg)
+    })
+}
+
+pub fn state_variable(_idx: usize, row: String) -> Result<StateVariable, std::io::Error> {
+    row.parse::<StateVariable>().map_err(|e| {
+        let msg = format!("failure decoding row {} due to: {:}", row, e);
+        std::io::Error::new(std::io::ErrorKind::InvalidData, msg)
+    })
+}
+
+/// used when reading boolean files into custom feature types, stored as a Box<[StateVariable]>
+/// see [`crate::model::traversal::default::custom::CustomTraversalEngine`].
+pub fn bool_to_state_variable(_idx: usize, row: String) -> Result<StateVariable, std::io::Error> {
+    let value = row.parse::<bool>().map_err(|e| {
+        let msg = format!("failure decoding row {} due to: {:}", row, e);
+        std::io::Error::new(std::io::ErrorKind::InvalidData, msg)
+    })?;
+    match value {
+        true => Ok(StateVariable(1.0)),
+        false => Ok(StateVariable(0.0)),
+    }
+}
+
+/// used when reading boolean files into custom feature types, stored as a Box<[StateVariable]>
+/// see [`crate::model::traversal::default::custom::CustomTraversalEngine`].
+pub fn i64_to_state_variable(_idx: usize, row: String) -> Result<StateVariable, std::io::Error> {
+    let value = row.parse::<i64>().map_err(|e| {
+        let msg = format!("failure decoding row {} due to: {:}", row, e);
+        std::io::Error::new(std::io::ErrorKind::InvalidData, msg)
+    })?;
+    Ok(StateVariable(value as f64))
+}
+
+/// used when reading boolean files into custom feature types, stored as a Box<[StateVariable]>
+/// see [`crate::model::traversal::default::custom::CustomTraversalEngine`].
+pub fn u64_to_state_variable(_idx: usize, row: String) -> Result<StateVariable, std::io::Error> {
+    let value = row.parse::<u64>().map_err(|e| {
+        let msg = format!("failure decoding row {} due to: {:}", row, e);
+        std::io::Error::new(std::io::ErrorKind::InvalidData, msg)
+    })?;
+    Ok(StateVariable(value as f64))
 }
