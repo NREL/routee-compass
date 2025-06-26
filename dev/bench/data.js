626d8c7ebaa1b03d7d09bd220ade4a10febd68b2
github-action-benchmark <github@users.noreply.github.com>
2025-06-26 19:07:11 +0000

    add Rust Benchmark (cargo) benchmark result for 9a105fc8839aec1df228fc35a9f12e641a80467c

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1750964831017,
+  "repoUrl": "https://github.com/NREL/routee-compass",
+  "entries": {
+    "Rust Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "126987327+brycemines@users.noreply.github.com",
+            "name": "brycemines",
+            "username": "brycemines"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "9a105fc8839aec1df228fc35a9f12e641a80467c",
+          "message": "Merge pull request #349 from NREL/bench-ci\n\nfix: switch benchmark running tool",
+          "timestamp": "2025-06-26T13:04:56-06:00",
+          "tree_id": "1c3992aceb59bcaec88c8bf6b6dd723154a51b6d",
+          "url": "https://github.com/NREL/routee-compass/commit/9a105fc8839aec1df228fc35a9f12e641a80467c"
+        },
+        "date": 1750964829468,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "routee-compass/downtown denver example",
+            "value": 7394538,
+            "range": "± 545414",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
