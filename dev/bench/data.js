dcd103f78a21f776cb3e829bf736583bdd698148
github-action-benchmark <github@users.noreply.github.com>
2025-07-11 16:46:29 +0000

    add Rust Benchmark (cargo) benchmark result for ec67c5440a36e4a669d1bc5f04f4467cb8791b83

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1752252389148,
+  "repoUrl": "https://github.com/NREL/routee-compass",
+  "entries": {
+    "Rust Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "nreinicke@users.noreply.github.com",
+            "name": "Nicholas Reinicke",
+            "username": "nreinicke"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "ec67c5440a36e4a669d1bc5f04f4467cb8791b83",
+          "message": "Merge pull request #360 from NREL/ndr/335-station-mapping\n\nUse spatial index to map stations at build time",
+          "timestamp": "2025-07-11T10:44:11-06:00",
+          "tree_id": "7adfa2eba81d8b379aef05e146e4e00f464945fa",
+          "url": "https://github.com/NREL/routee-compass/commit/ec67c5440a36e4a669d1bc5f04f4467cb8791b83"
+        },
+        "date": 1752252387568,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "routee-compass/downtown denver example",
+            "value": 7596777,
+            "range": "± 155757",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
