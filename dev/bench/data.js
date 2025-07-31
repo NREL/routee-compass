64779f5f648fee170c89f0455a658616b2c56539
brycemines <brycemines@users.noreply.github.com>
2025-07-31 15:48:26 +0000

    deploy: 1f326f952aa97def4d1859433fb1e0b5209e92dc

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,8 +1,128 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1753461227269,
+  "lastUpdate": 1753976528297,
   "repoUrl": "https://github.com/NREL/routee-compass",
   "entries": {
     "Rust Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "126987327+brycemines@users.noreply.github.com",
+            "name": "brycemines",
+            "username": "brycemines"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "9a105fc8839aec1df228fc35a9f12e641a80467c",
+          "message": "Merge pull request #349 from NREL/bench-ci\n\nfix: switch benchmark running tool",
+          "timestamp": "2025-06-26T13:04:56-06:00",
+          "tree_id": "1c3992aceb59bcaec88c8bf6b6dd723154a51b6d",
+          "url": "https://github.com/NREL/routee-compass/commit/9a105fc8839aec1df228fc35a9f12e641a80467c"
+        },
+        "date": 1750964829468,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "routee-compass/downtown denver example",
+            "value": 7394538,
+            "range": "± 545414",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "yaessus@ncsu.edu",
+            "name": "Yamil Essus",
+            "username": "yamilbknsu"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b57253bc1ba0c9f7fe952fca8d2c2f75a3fdaa82",
+          "message": "Merge pull request #353 from NREL/yep/gzip-output\n\nImplemented option to return a compressed file when the provided filename ends in `.gz`",
+          "timestamp": "2025-07-08T13:37:38-04:00",
+          "tree_id": "97eaffe04e11987d25532140be201e7d576b84d3",
+          "url": "https://github.com/NREL/routee-compass/commit/b57253bc1ba0c9f7fe952fca8d2c2f75a3fdaa82"
+        },
+        "date": 1751996386872,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "routee-compass/downtown denver example",
+            "value": 7438683,
+            "range": "± 587676",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "nreinicke@users.noreply.github.com",
+            "name": "Nicholas Reinicke",
+            "username": "nreinicke"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "c375394859e06051f199e611162f1830260ffa20",
+          "message": "Merge pull request #350 from NREL/ndr/bev-charging\n\nBEV charging",
+          "timestamp": "2025-07-08T16:01:39-06:00",
+          "tree_id": "e6a0cdcfc7036867c9f4a21e9f6d4094d0de66f6",
+          "url": "https://github.com/NREL/routee-compass/commit/c375394859e06051f199e611162f1830260ffa20"
+        },
+        "date": 1752012228021,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "routee-compass/downtown denver example",
+            "value": 7619081,
+            "range": "± 169042",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "nreinicke@users.noreply.github.com",
+            "name": "Nicholas Reinicke",
+            "username": "nreinicke"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "d7895762c2dfb174d8767203df8638ebff78b845",
+          "message": "Merge pull request #354 from NREL/ndr/fix-docs\n\nFix docs",
+          "timestamp": "2025-07-10T09:09:12-06:00",
+          "tree_id": "f8dbaa654619742be5ed5b2fea666cf0862d420e",
+          "url": "https://github.com/NREL/routee-compass/commit/d7895762c2dfb174d8767203df8638ebff78b845"
+        },
+        "date": 1752160280235,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "routee-compass/downtown denver example",
+            "value": 7363681,
+            "range": "± 118264",
+            "unit": "ns/iter"
+          }
+        ]
+      },
       {
         "commit": {
           "author": {
@@ -96,9 +216,9 @@ window.BENCHMARK_DATA = {
       {
         "commit": {
           "author": {
-            "email": "robert.fitzgerald@nrel.gov",
-            "name": "Rob Fitzgerald",
-            "username": "robfitzgerald"
+            "email": "126987327+brycemines@users.noreply.github.com",
+            "name": "brycemines",
+            "username": "brycemines"
           },
           "committer": {
             "email": "noreply@github.com",
@@ -106,19 +226,19 @@ window.BENCHMARK_DATA = {
             "username": "web-flow"
           },
           "distinct": true,
-          "id": "92f8045fad7ca722313e75f0f40c3a4764c03b7c",
-          "message": "Merge pull request #367 from NREL/hotfix/smartcore-version\n\nhold smartcore at =0.4.0",
-          "timestamp": "2025-07-25T10:31:38-06:00",
-          "tree_id": "519838ceca1fd47155dbc875d8346010e4082562",
-          "url": "https://github.com/NREL/routee-compass/commit/92f8045fad7ca722313e75f0f40c3a4764c03b7c"
+          "id": "1f326f952aa97def4d1859433fb1e0b5209e92dc",
+          "message": "Merge pull request #365 from NREL/bai/fix-bench-ci\n\nfix: deploy-docs and rust-bench GA no longer conflict",
+          "timestamp": "2025-07-31T09:39:37-06:00",
+          "tree_id": "119aa37e92af5af3d5f1ea092292d8e80103e918",
+          "url": "https://github.com/NREL/routee-compass/commit/1f326f952aa97def4d1859433fb1e0b5209e92dc"
         },
-        "date": 1753461225681,
+        "date": 1753976527377,
         "tool": "cargo",
         "benches": [
           {
             "name": "routee-compass/downtown denver example",
-            "value": 7382290,
-            "range": "± 152530",
+            "value": 7482073,
+            "range": "± 521060",
             "unit": "ns/iter"
           }
         ]
