403c95fc7baa00e8f367a6c3ac8f033cb5cbaf60
Rob Fitzgerald <robert.fitzgerald@nrel.gov>
2025-08-13 10:08:48 -0600

    cost model config type

--- rust/routee-compass-core/src/model/cost/mod.rs
+++ rust/routee-compass-core/src/model/cost/mod.rs
@@ -6,7 +6,9 @@ pub mod cost_model_service;
 pub mod cost_ops;
 pub mod network;
 mod vehicle;
+mod cost_model_config;
 
+pub use cost_model_config::CostModelConfig;
 pub use cost_aggregation::CostAggregation;
 pub use cost_model::CostModel;
 pub use cost_model_error::CostModelError;
