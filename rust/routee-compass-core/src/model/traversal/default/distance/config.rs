e0a9b87ee9673fe04aa3e5d9dc57fd3db67dd9d6
Rob Fitzgerald <robert.fitzgerald@nrel.gov>
2025-09-30 14:16:10 -0600

    missing DistanceUnit configuration

---
+++ rust/routee-compass-core/src/model/traversal/default/distance/config.rs
@@ -0,0 +1,9 @@
+use serde::{Deserialize, Serialize};
+
+use crate::model::unit::DistanceUnit;
+
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct DistanceTraversalConfig {
+    pub distance_unit: Option<DistanceUnit>
+}   
