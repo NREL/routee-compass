97fd31294420d7ece2ffd04099f131f8b306b42e
github-action-benchmark <github@users.noreply.github.com>
2025-10-03 16:40:38 +0000

    add Rust Benchmark (cargo) benchmark result for 986187b19b6ba330ad562b370c46bc2e127ca289

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1759265817446,
+  "lastUpdate": 1759509638394,
   "repoUrl": "https://github.com/NREL/routee-compass",
   "entries": {
     "Rust Benchmark": [
@@ -719,6 +719,36 @@ window.BENCHMARK_DATA = {
             "name": "routee-compass/downtown denver example",
             "value": 7839010,
             "range": "± 483680",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "robert.fitzgerald@nrel.gov",
+            "name": "Rob Fitzgerald",
+            "username": "robfitzgerald"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "986187b19b6ba330ad562b370c46bc2e127ca289",
+          "message": "Merge pull request #396 from NREL/rjf/departure-access\n\nexpand TraversalModel scope and remove AccessModel",
+          "timestamp": "2025-10-03T10:38:21-06:00",
+          "tree_id": "e43a85d76c6b515d7a45856d0355af0e74b529d7",
+          "url": "https://github.com/NREL/routee-compass/commit/986187b19b6ba330ad562b370c46bc2e127ca289"
+        },
+        "date": 1759509637974,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "routee-compass/downtown denver example",
+            "value": 6609820,
+            "range": "± 124423",
             "unit": "ns/iter"
           }
         ]
