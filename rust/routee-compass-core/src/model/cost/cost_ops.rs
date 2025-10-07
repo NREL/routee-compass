06703b259bfebc56e93fdd40ce066a603bd33fa4
Rob Fitzgerald <robert.fitzgerald@nrel.gov>
2025-10-07 15:31:52 -0600

    typo

--- rust/routee-compass-core/src/model/cost/cost_ops.rs
+++ rust/routee-compass-core/src/model/cost/cost_ops.rs
@@ -21,7 +21,7 @@ pub fn describe_cost_feature_configuration(
         (None, Some(v), None) => format!("Feature '{name}' was provided cost rate '{v}' but no weight, so it will not contribute to the cost model."),
         (Some(w), None, None) => format!("Feature '{name}' was provided weight {w} but no cost rate. The default rate is zero, so this feature will be zeroed out and will not contribute to the cost model."),
         (Some(w), Some(v), None) => format!("Feature '{name}' was provided weight {w} and vehicle rate {v} and will contribute to the cost model. No network costs were provided for this feature."),
-        (None, None, Some(_)) => format!("Feature '{name}' with network rate will not contribute to cost model has it has no weight."),
+        (None, None, Some(_)) => format!("Feature '{name}' with network rate will not contribute to cost model because it has no weight."),
         (None, Some(v), Some(_)) => format!("Feature '{name}' was provided vehicle cost rate '{v}' and network rate, but has no weight value, so it will not contribute to the cost model."),
         (Some(w), None, Some(_)) => format!("Feature '{name}' was provided weight {w} and network rate input, will contribute to the cost model."),
         (Some(w), Some(v), Some(_)) => format!("Feature '{name}' was provided weight {w}, vehicle rate {v} and network rate. It will contribute to the cost model."),
