857148be0d6b8115d487c1ea81cf32e6390746eb
Rob Fitzgerald <robert.fitzgerald@nrel.gov>
2025-11-04 15:37:06 -0700

    write header once. un-comment writemode as argument to output policies.

--- rust/routee-compass/src/app/compass/response/internal_writer.rs
+++ rust/routee-compass/src/app/compass/response/internal_writer.rs
@@ -1,27 +1,47 @@
 use flate2::write::GzEncoder;
-use std::{fs::File, io::Result, io::Write};
+use std::{fs::File, io::Write};
 
 use crate::app::compass::{
     response::response_output_format::ResponseOutputFormat, CompassAppError,
 };
 
 pub enum InternalWriter {
+    /// writes to a file without encryption
     File { file: File },
+    /// writes to a gzipped file
     GzippedFile { encoder: GzEncoder<File> },
 }
 
 impl InternalWriter {
+    /// writes the header to the file. checks if the file is empty; in the case that this 
+    /// was invoked in a chunking run of Compass, the file could already contain rows and 
+    /// we therefore want to skip writing the header.
     pub fn write_header(
         &mut self,
         format: &ResponseOutputFormat,
     ) -> core::result::Result<(), CompassAppError> {
-        let header = format
-            .initial_file_contents()
-            .unwrap_or_else(|| String::from(""));
+        if !self.is_empty()? {
+            let header = format
+                .initial_file_contents()
+                .unwrap_or_else(|| String::from(""));
+    
+            self.write(header.as_bytes()).map(|_| {}).map_err(|e| {
+                CompassAppError::InternalError(format!("Failure writing header to file: {e}"))
+            })?;
+        }
+        Ok(())
+    }
 
-        self.write(header.as_bytes()).map(|_| {}).map_err(|e| {
-            CompassAppError::InternalError(format!("Failure writing header to file: {e}"))
-        })
+    /// test if file has not yet received any data
+    pub fn is_empty(&self) -> Result<bool, CompassAppError> {
+        match self {
+            InternalWriter::File { file } => {
+                is_empty(file)
+            }
+            InternalWriter::GzippedFile { encoder } => {
+                is_empty(encoder.get_ref())
+            }
+        }
     }
 
     pub fn finish(&mut self) -> core::result::Result<(), CompassAppError> {
@@ -46,17 +66,22 @@ impl InternalWriter {
 }
 
 impl Write for InternalWriter {
-    fn write(&mut self, buf: &[u8]) -> Result<usize> {
+    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
         match self {
             InternalWriter::File { file } => file.write(buf),
             InternalWriter::GzippedFile { encoder } => encoder.write(buf),
         }
     }
 
-    fn flush(&mut self) -> Result<()> {
+    fn flush(&mut self) -> std::io::Result<()> {
         match self {
             InternalWriter::File { file } => file.flush(),
             InternalWriter::GzippedFile { encoder } => encoder.flush(),
         }
     }
+}
+
+fn is_empty(file: &File) -> Result<bool, CompassAppError> {
+    let m = file.metadata().map_err(|e| CompassAppError::InternalError(format!("failure inspecting output file metadata: {e}")))?;
+    Ok(m.len() == 0)
 }
